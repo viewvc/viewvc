97b63feb0fd1fecf1b6b868d0de8effe2e5b0559
Greg Stein <gstein@users.noreply.github.com>
2001-05-09 09:23:00 +0000

    commit a checkpoint, with some tools, before doing some heavy lifting

--- lib/rcsparse.py
+++ lib/rcsparse.py
@@ -240,6 +240,10 @@ class Parser:
     self.parse_rcs_description()
     self.parse_rcs_deltatext()
 
+    # easiest for us to tell the sink it is done, rather than worry about
+    # higher level software doing it.
+    self.sink.parse_completed()
+
     self.rcsfile = self.sink = None
 
 
@@ -261,6 +265,13 @@ class Sink:
     pass
   def tree_completed(self):
     pass
+  def parse_completed(self):
+    pass
+
+# --------------------------------------------------------------------------
+#
+# TESTING AND DEBUGGING TOOLS
+#
 
 class DebugSink:
   def set_head_revision(self, revision):
@@ -291,3 +302,76 @@ class DebugSink:
     print 'revision:', revision
     print '    log:', log
     print '    text:', text[:100], '...'
+
+class DumpSink:
+  """Dump all the parse information directly to stdout.
+
+  The output is relatively unformatted and untagged. It is intended as a
+  raw dump of the data in the RCS file. A copy can be saved, then changes
+  made to the parsing engine, then a comparison of the new output against
+  the old output.
+  """
+  def __init__(self):
+    global sha
+    import sha
+
+  def set_head_revision(self, revision):
+    print revision
+
+  def set_principal_branch(self, branch_name):
+    print branch_name
+
+  def define_tag(self, name, revision):
+    print name, revision
+
+  def set_comment(self, comment):
+    print comment
+
+  def set_description(self, description):
+    print description
+
+  def define_revision(self, revision, timestamp, author, state,
+                      branches, next):
+    print revision, timestamp, author, state, branches, next
+
+  def set_revision_info(self, revision, log, text):
+    print revision, sha.new(log).hexdigest(), sha.new(text).hexdigest()
+
+  def tree_completed(self):
+    print 'tree_completed'
+
+  def parse_completed(self):
+    print 'parse_completed'
+
+def dump_file(fname):
+  Parser().parse(open(fname), DumpSink())
+
+def time_file(fname):
+  import time
+  p = Parser().parse
+  f = open(fname)
+  s = Sink()
+  t = time.time()
+  p(f, s)
+  t = time.time() - t
+  print t
+
+def _usage():
+  print 'This is normally a module for importing, but it has a couple'
+  print 'features for testing as an executable script.'
+  print 'USAGE: %s COMMAND filename,v' % sys.argv[0]
+  print '  where COMMAND is one of:'
+  print '    dump: filename is "dumped" to stdout'
+  print '    time: filename is parsed with the time written to stdout'
+  sys.exit(1)
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv) != 3:
+    usage()
+  if sys.argv[1] == 'dump':
+    dump_file(sys.argv[2])
+  elif sys.argv[1] == 'time':
+    time_file(sys.argv[2])
+  else:
+    usage()
