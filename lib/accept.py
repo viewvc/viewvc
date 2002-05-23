593a0e9e6011feecdde4ca7b0c3c5a2e296e2831
Greg Stein <gstein@users.noreply.github.com>
2002-05-23 19:13:40 +0000

    Problem found by Daniel Rall <dlr@collab.net>:     Netscape 4.x and early Mozillas would not send qvalues with their     language tags. Even for a simple case (browser sends "en, ja" and     we have en and ja available), we could produce unexpected (read:     erroneous) results.
    Thus, we now select from multiple matches based on the ordering of the
    items in the Accept-Language: header.
    
    I've also added more comments to this code since some of it is rather
    opaque.

--- lib/accept.py
+++ lib/accept.py
@@ -111,6 +111,23 @@ class _LanguageRange(_AcceptItem):
     return None
 
 class _LanguageSelector:
+  """Instances select an available language based on the user's request.
+
+  Languages found in the user's request are added to this object with the
+  append() method (they should be instances of _LanguageRange). After the
+  languages have been added, then the caller can use select_from() to
+  determine which user-request language(s) best matches the set of
+  available languages.
+
+  Strictly speaking, this class is pretty close for more than just
+  language matching. It has been implemented to enable q-value based
+  matching between requests and availability. Some minor tweaks may be
+  necessary, but simply using a new 'item_class' should be sufficient
+  to allow the _parse() function to construct a selector which holds
+  the appropriate item implementations (e.g. _LanguageRange is the
+  concrete _AcceptItem class that handles matching of language tags).
+  """
+
   item_class = _LanguageRange
 
   def __init__(self):
@@ -120,7 +137,8 @@ class _LanguageSelector:
     """Select one of the available choices based on the request.
 
     Note: if there isn't a match, then the first available choice is
-    considered the default.
+    considered the default. Also, if a number of matches are equally
+    relevant, then the first-requested will be used.
 
     avail is a list of language-tag strings of available languages
     """
@@ -140,7 +158,8 @@ class _LanguageSelector:
         qvalue = want.matches(tag)
         #print 'have %s. want %s. qvalue=%s' % (tag, want.name, qvalue)
         if qvalue is not None and len(want.name) > longest:
-          # we have a match and it is longer than any we may have had
+          # we have a match and it is longer than any we may have had.
+          # the final qvalue should be from this tag.
           final = qvalue
           longest = len(want.name)
 
@@ -148,22 +167,48 @@ class _LanguageSelector:
       if final:
         matches.append((final, tag))
 
-    # if we have any matches, then look at the highest qvalue
-    if matches:
-      matches.sort()
-      qvalue, tag = matches[-1]
+    # if there are no matches, then return the default language tag
+    if not matches:
+      return avail[0]
 
-      if len(matches) >= 2 and matches[-2][0] == qvalue:
-        #print "non-deterministic choice", avail
-        pass
+    # get the highest qvalue and its corresponding tag
+    matches.sort()
+    qvalue, tag = matches[-1]
 
-      # if the qvalue is non-zero, then we have a valid match
-      if qvalue:
-        return tag
-      # the qvalue is zero (non-match). drop thru to return the default
+    # if the qvalue is zero, then we have no valid matches. return the
+    # default language tag.
+    if not qvalue:
+      return avail[0]
 
-    # return the default language tag
-    return avail[0]
+    # if there are two or more matches, and the second-highest has a
+    # qvalue equal to the best, then we have multiple "best" options.
+    # select the one that occurs first in self.requested
+    if len(matches) >= 2 and matches[-2][0] == qvalue:
+      # remove non-best matches
+      while matches[0][0] != qvalue:
+        del matches[0]
+      #print "non-deterministic choice", matches
+
+      # sequence through self.requested, in order
+      for want in self.requested:
+        # try to find this one in our best matches
+        for qvalue, tag in matches:
+          if want.matches(tag):
+            # this requested item is one of the "best" options
+            ### note: this request item could match *other* "best" options,
+            ### so returning *this* one is rather non-deterministic.
+            ### theoretically, we could go further here, and do another
+            ### search based on the ordering in 'avail'. however, note
+            ### that this generally means that we are picking from multiple
+            ### *SUB* languages, so I'm all right with the non-determinism
+            ### at this point. stupid client should send a qvalue if they
+            ### want to refine.
+            return tag
+
+      # NOTREACHED
+
+    # return the best match
+    return tag
 
   def append(self, item):
     self.requested.append(item)
@@ -176,6 +221,10 @@ def _test():
   assert s.select_from(['en']) == 'en'
   assert s.select_from(['en', 'de']) == 'en'
   assert s.select_from(['de', 'en']) == 'en'
+
+  # Netscape 4.x and early version of Mozilla may not send a q value
+  s = language('en, ja')
+  assert s.select_from(['en', 'ja']) == 'en'
 
   s = language('fr, de;q=0.9, en-gb;q=0.7, en;q=0.6, en-gb-foo;q=0.8')
   assert s.select_from(['en']) == 'en'
