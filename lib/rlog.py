f7e2e51c9c94971be77d36ae32cd4245bd2b81c5
Greg Stein <gstein@users.noreply.github.com>
2002-09-05 07:18:30 +0000

    Rip out the rlog parser that was in this file. Leverage the parser from vclib.bincvs instead, and apply a small compatibility layer over the top of it. tests/testparse.py was used to verify that the new parser/wrapper is equivalent.
    [ things can be simplified further by changing the callers to not
      require the compat wrapper ]

--- lib/rlog.py
+++ lib/rlog.py
@@ -15,20 +15,8 @@
 
 import os
 import string
-import re
-import time
-import compat
 
-
-## RLogOutputParser uses the output of rlog to build a list of Commit
-## objects describing all the checkins from a given RCS file; this
-## parser is fairly optimized, and therefore can be delicate if the
-## rlog output varies between versions of rlog; I don't know if it does;
-## to make really fast, I should wrap the C rcslib
-
-## there's definately not much error checking here; I'll assume things
-## will go smoothly, and trap errors via exception handlers above this
-## function
+from vclib import bincvs
 
 ## exception for this class
 error = 'rlog error'
@@ -46,12 +34,7 @@ class RLogData:
         index = string.rfind(rlog_entry.revision, '.')
         branch_revision = rlog_entry.revision[:index]
 
-        try:
-            branch = self.symbolic_name_hash[branch_revision]
-        except KeyError:
-            branch = ''
-
-        return branch
+        return self.symbolic_name_hash.get(branch_revision, '')
     
 
 class RLogEntry:
@@ -78,256 +61,85 @@ class RLogEntry:
 ##         self.type = RLogEntry.CHANGE
 
 
-class RLog:
-    "Provides a alternative file-like interface for running 'rlog'."
-    
-    def __init__(self, cfg, filename, revision, date):
-        self.filename = self.fix_filename(filename)
-        self.checkout_filename = self.create_checkout_filename(self.filename)
-        self.revision = revision
-        self.date = date
+def _get_v_file(filename):
+    # all RCS files have the ",v" ending
+    if filename[-2:] != ",v":
+        filename = filename + ',v'
 
-        arg_list = []
-        if self.revision:
-            arg_list.append('-r%s' % (self.revision))
-        if self.date:
-            arg_list.append('-d%s' % (self.date))
+    if os.path.isfile(filename):
+        return filename
 
-        temp = os.path.join(cfg.general.rcs_path, "rlog")
-        self.cmd = '%s %s "%s"' % (temp, string.join(arg_list), self.filename)
-        self.rlog = os.popen(self.cmd, 'r')
+    # check the Attic for the RCS file
+    path, basename = os.path.split(filename)
+    filename = os.path.join(path, "Attic", basename)
 
-    def fix_filename(self, filename):
-        ## all RCS files have the ",v" ending
-        if filename[-2:] != ",v":
-            filename = "%s,v" % (filename)
+    if os.path.isfile(filename):
+        return filename
 
-        if os.path.isfile(filename):
-            return filename
+    ### create an exception class
+    raise error, "rlog file not found: %s" % (filename)
 
-        ## check the Attic for the RCS file
-        path, basename = os.path.split(filename)
-        filename = os.path.join(path, "Attic", basename)
+def _get_co_file(v_file):
+    # remove the ",v" suffix
+    co_file = v_file[:-2]
 
-        if os.path.isfile(filename):
-            return filename
-        
-        raise error, "rlog file not found: %s" % (filename)
+    # look for, and remove, any Attic component
+    path, basename = os.path.split(co_file)
+    if path[-6:] == '/Attic':
+        return os.path.join(path[:-6], basename)
 
-    def create_checkout_filename(self, filename):
-        ## cut off the ",v"
-        checkout_filename = filename[:-2]
+    return co_file
 
-        ## check if the file is in the Attic
-        path, basename = os.path.split(checkout_filename)
-        if path[-6:] != '/Attic':
-            return checkout_filename
+def GetRLogData(cfg, path, revision=''):
+    v_file = _get_v_file(path)
+    head, branch, taginfo, revs = bincvs.fetch_log(cfg.general.rcs_path,
+                                                   v_file,
+                                                   revision)
+    class _blank:
+      pass
 
-        ## remove the "Attic" part of the path
-        checkout_filename = os.path.join(path[:-6], basename)
-        return checkout_filename
-        
-    def readline(self):
-        try:
-            line = self.rlog.readline()
-        except AttributeError:
-            self.error()
-            
-        if line:
-            return line
+    data = RLogData(_get_co_file(v_file))
 
-        status = self.close()
-        if status:
-            self.error()
+    for name, rev in taginfo.items():
+        # if this is a branch (X.Y.0.Z), then remove the .0 portion
+        idx = string.rfind(rev, '.')
+        if rev[idx-2:idx] == '.0':
+            rev = rev[:idx-2] + rev[idx:]
 
-        return None
+        ### hmm. this is only useful for *branch names*, which won't overlap
+        ### on a specific rev.
+        ### note: the tag name associated with a revision will be
+        ### non-deterministic because of this overlap.
+        ### maybe just omit the name for non-branch tags?
+        data.symbolic_name_hash[rev] = name
 
-    def close(self):
-        status = self.rlog.close()
-        self.rlog = None
-        return status
+    for entry in revs:
+        new_entry = _blank()
 
+        if entry.changed:
+            # extract the plus/minus and drop the sign
+            plus, minus = string.split(entry.changed)
+            new_entry.pluscount = plus[1:]
+            new_entry.minuscount = minus[1:]
 
-    def error(self):
-        raise error, "unexpected rlog exit: %s" % (self.cmd)
-
-
-## constants used in the output parser
-
-_rlog_commit_sep = '----------------------------\n'
-_rlog_end = '=============================================================================\n'
-
-## regular expression used in the output parser
-_re_symbolic_name = re.compile("\s+([^:]+):\s+(.+)$")
-
-_re_revision = re.compile("^revision\s+([0-9.]+).*")
-
-_re_data_line = re.compile(
-    "^date:\s+(\d+)/(\d+)/(\d+)\s+(\d+):(\d+):(\d+);\s+"\
-    "author:\s+([^;]+);\s+"\
-    "state:\s+([^;]+);\s+"\
-    "lines:\s+\+(\d+)\s+\-(\d+)$")
-
-_re_data_line_add = re.compile(
-    "^date:\s+(\d+)/(\d+)/(\d+)\s+(\d+):(\d+):(\d+);\s+"\
-    "author:\s+([^;]+);\s+"\
-    "state:\s+([^;]+);$")
-
-class RLogOutputParser:
-    
-    def __init__(self, rlog):
-        self.rlog = rlog
-        self.rlog_data = RLogData(rlog.checkout_filename)
-
-        ## run the parser
-        self.parse_to_symbolic_names()
-        self.parse_symbolic_names()
-        self.parse_to_description()
-        self.parse_rlog_entries()
-    
-    def parse_to_symbolic_names(self):
-        while 1:
-            line = self.rlog.readline()
-            if line[:15] == 'symbolic names:':
-                break
-
-    def parse_symbolic_names(self):
-        ## parse all the tags int the branch_hash, it's used later to get
-        ## the text names of non-head branches
-        while 1:
-            line = self.rlog.readline()
-            match = _re_symbolic_name.match(line)
-            if not match:
-                break
-
-            (tag, revision) = match.groups()
-
-            ## check if the tag represents a branch, in RCS this means
-            ## the second-to-last number is a zero
-            index = string.rfind(revision, '.')
-            if revision[index-2:index] == '.0':
-                revision = revision[:index-2] + revision[index:]
-            
-            self.rlog_data.symbolic_name_hash[revision] = tag
-
-    def parse_to_description(self):
-        while 1:
-            line = self.rlog.readline()
-            if line[:12] == 'description:':
-                break
-            
-        ## eat all lines until we reach '-----' seperator
-        while 1:
-            line = self.rlog.readline()
-            if line == _rlog_commit_sep:
-                break
-
-    def parse_rlog_entries(self):
-        while 1:
-            rlog_entry = self.parse_one_rlog_entry()
-            if not rlog_entry:
-                break
-            self.rlog_data.rlog_entry_list.append(rlog_entry)
-
-    def parse_one_rlog_entry(self):
-        ## revision line/first line
-        line = self.rlog.readline()
-	# Since FreeBSD's rlog outputs extra "---...---\n" before
-	# "===...===\n", _rlog_end may be occured here.
-        if not line or line == _rlog_end:
-            return None
-
-        ## revision
-        match = _re_revision.match(line)
-        (revision,) = match.groups()
-
-        ## data line
-        line = self.rlog.readline()
-        match = _re_data_line.match(line)
-        if not match:
-            match = _re_data_line_add.match(line)
-
-        if not match:
-            raise error, "bad rlog parser, no cookie!"
-
-        ## retrieve the matched grops as a tuple in hopes
-        ## this will be faster (ala profiler)
-        groups = match.groups()
-
-        year = string.atoi(groups[0])
-        month = string.atoi(groups[1])
-        day = string.atoi(groups[2])
-        hour = string.atoi(groups[3])
-        minute = string.atoi(groups[4])
-        second = string.atoi(groups[5])
-        author = groups[6]
-        state = groups[7]
-
-        ## very strange; here's the deal: if this is a newly added file,
-        ## then there is no plus/minus count count of lines; if there
-        ## is, then this could be a "CHANGE" or "REMOVE", you can tell
-        ## if the file has been removed by looking if state == 'dead'
-        try:
-            pluscount = groups[8]
-            minuscount = groups[9]
-        except IndexError:
-            pluscount = ''
-            minuscount = ''
-            cmit_type = RLogEntry.ADD
+            if entry.state == 'dead':
+                new_entry.type = RLogEntry.REMOVE
+            else:
+                new_entry.type = RLogEntry.CHANGE
         else:
-            if state == 'dead':
-                cmit_type = RLogEntry.REMOVE
-            else:
-                cmit_type = RLogEntry.CHANGE
-        
-        ## branch line: pretty much ignored if it's there
-        desc_line_list = []
-                
-        line = self.rlog.readline()
-        if not line[:10] == 'branches: ':
-            desc_line_list.append(string.rstrip(line))
+            new_entry.type = RLogEntry.ADD
+            new_entry.pluscount = new_entry.minuscount = ''
 
-        ## suck up description
-        while 1:
-            line = self.rlog.readline()
+        # some goop to drop a trailing newline
+        if entry.log[-1:] == '\n':
+          new_entry.description = entry.log[:-1]
+        else:
+          new_entry.description = entry.log
 
-            ## the last line printed out by rlog is '===='...
-            ## or '------'... between entries
-            if line == _rlog_commit_sep or line == _rlog_end:
-                break
+        new_entry.revision = entry.rev
+        new_entry.author = entry.author
+        new_entry.time = entry.date
 
-            ## append line to the descripton list
-            desc_line_list.append(string.rstrip(line))
+        data.rlog_entry_list.append(new_entry)
 
-        ## compute time using time routines in seconds from epoc GMT
-        # timegm needs a four digit date. Be paranoid about it.
-        EPOCH = 1970
-        if year < EPOCH:
-            if year < 70:
-                year = year + 2000
-            else:
-                year = year + 1900
-            if year < EPOCH:
-                raise ValueError, 'invalid year'
-        gmt_time = \
-            compat.timegm((year, month, day, hour, minute, second, 0, 0, 0))
-
-        ## now create and return the RLogEntry
-        rlog_entry = RLogEntry()
-        rlog_entry.type = cmit_type
-        rlog_entry.revision = revision
-        rlog_entry.author = author
-        rlog_entry.description = string.join(desc_line_list, '\n')
-        rlog_entry.time = gmt_time
-        rlog_entry.pluscount = pluscount
-        rlog_entry.minuscount = minuscount
-
-        return rlog_entry
-
-
-## entrypoints
-
-def GetRLogData(cfg, path, revision = '', date = ''):
-    rlog = RLog(cfg, path, revision, date)
-    rlog_parser = RLogOutputParser(rlog)
-    return rlog_parser.rlog_data
+    return data
