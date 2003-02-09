16fd92fe2e4a2f7a9b8a966e3f085588225096d9
rey4 <rey4@8cb11bc2-c004-0410-86c3-e597b4017df7>
2003-02-09 09:57:46 +0000

    - modified popen module to work on windows - added debugging mode for popen which can display the input and   output from child processes (only works on windows for now)

--- trunk/lib/debug.py
+++ trunk/lib/debug.py
@@ -16,7 +16,10 @@
 #       the lambda form (when debugging is disabled) should be even faster.
 #
 
-if 0:
+SHOW_TIMES = 0
+SHOW_CHILD_PROCESSES = 0
+
+if SHOW_TIMES:
 
   import time
 
@@ -40,3 +43,108 @@ if 0:
 else:
 
   t_start = t_end = dump = lambda *args: None
+
+class ViewcvsException:
+  def __init__(self, msg, httpCode = None):
+    self.msg = msg
+    self.httpCode = httpCode
+    s = "<p><pre>"
+    s += self.msg
+    s += "</pre></p>\n"
+
+    if self.httpCode:
+      s += "<h4>HTTP-like status code:</h4>\n<p><pre>\n"
+      s += self.httpCode
+      s += "</pre></p><hr>\n"
+    
+    self.description = s
+
+  def __str__(self):
+    return "ViewCVS Unrecoverable Error"
+
+def PrintStackTrace(text = ""):
+  import traceback
+  import string
+  import sapi
+  print "<hr><p><font color=red>", text, "</font></p>\n<p><pre>"
+  print sapi.server.escape(string.join(traceback.format_stack(), ''))
+  print "</pre></p>"
+  sys.stdout.flush()
+
+def PrintException():
+  import sys
+  import traceback
+  import string
+  import sapi
+  
+  sapi.server.header()  
+  print "<h3>Exception</h3>"
+  info = sys.exc_info()
+  
+  # put message in a more prominent position (rather than at the end of the stack trace)
+  if isinstance(info[1], ViewcvsException):
+    print "<h4>ViewCVS Messages:</h4>"
+    print info[1].description
+  
+  print "<h4>Python Messages:</h4>\n<p><pre>"
+  print sapi.server.escape(string.join(apply(traceback.format_exception, info), ''))
+  print "</pre></p>"
+  del info
+
+if SHOW_CHILD_PROCESSES:
+  class Process:
+    def __init__(self, command, inStream, outStream, errStream):
+      self.command = command
+      self.debugIn = inStream
+      self.debugOut = outStream
+      self.debugErr = errStream
+  
+    def printInfo(self):
+      print "Command Line", command
+
+  def DumpChildren():
+    import sapi, sys
+    server = sapi.server.self()
+    
+    lastOut = None
+    
+    server.header()
+    i = 0
+    for k in server.processes:
+      i += 1
+      print "<div align=center>Child Process", i, "</div>"
+      print "<table border=1>"
+      sys.stdout.write("<tr>\n  <td valign=top>Command Line</td>  <td><pre>")
+      sys.stdout.write(server.escape(k.command))
+      sys.stdout.write("</pre></td>\n</tr>\n")
+      sys.stdout.write("<tr>\n  <td valign=top>Standard In:</td>  <td>")
+      if k.debugIn is lastOut and not (lastOut is None):
+        sys.stdout.write("<i>Output from process %i</i>" % (i - 1))
+      elif k.debugIn:
+        sys.stdout.write("<pre>")
+        sys.stdout.write(server.escape(k.debugIn.getvalue()))
+        sys.stdout.write("</pre>")
+      sys.stdout.write("</td>\n</tr>\n")
+      
+      if k.debugOut is k.debugErr:
+        sys.stdout.write("<tr>\n  <td valign=top>Standard Out & Error:</td>  <td><pre>")
+        if k.debugOut:
+          sys.stdout.write(server.escape(k.debugOut.getvalue()))
+        sys.stdout.write("</pre></td>\n</tr>\n")
+      else:
+        sys.stdout.write("<tr>\n  <td valign=top>Standard Out:</td>  <td><pre>")
+        if k.debugOut:
+          sys.stdout.write(server.escape(k.debugOut.getvalue()))
+        sys.stdout.write("</pre></td>\n</tr>\n")
+        sys.stdout.write("<tr>\n  <td valign=top>Standard Error:</td>  <td><pre>")
+        if k.debugErr:
+          sys.stdout.write(server.escape(k.debugErr.getvalue()))
+        sys.stdout.write("</pre></td>\n</tr>\n")
+
+      sys.stdout.write("</table>\n")
+      lastOut = k.debugOut
+else:
+
+  def DumpChildren():
+    pass
+    
--- trunk/lib/popen.py
+++ trunk/lib/popen.py
@@ -23,8 +23,59 @@
 
 import os
 import sys
+import sapi
+
+if sys.platform == "win32":
+  import win32popen
+  import win32event
+  import win32process
+  import debug
+  import StringIO
 
 def popen(cmd, args, mode, capture_err=1):
+  if sys.platform == "win32":
+    
+    command = win32popen.CommandLine(cmd, args)
+
+    if mode.find('r') >= 0:
+      hStdIn = None        
+      
+      if debug.SHOW_CHILD_PROCESSES:
+        dbgIn, dbgOut = None, StringIO.StringIO()
+
+        handle, hStdOut = win32popen.MakeSpyPipe(0, 1, (dbgOut,))
+
+        if capture_err:
+          hStdErr = hStdOut
+          dbgErr = dbgOut
+        else:
+          dbgErr = StringIO.StringIO()
+          x, hStdErr = win32popen.MakeSpyPipe(None, 1, (dbgErr,))  
+      else:
+        handle, hStdOut = win32popen.CreatePipe(0, 1, 1, 1)
+        if capture_err:
+          hStdErr = hStdOut
+        else:
+          hStdErr = None
+
+    else:
+      if debug.SHOW_CHILD_PROCESSES:
+        dbgIn, dbgOut, dbgErr = StringIO.StringIO(), StringIO.StringIO(), StringIO.StringIO()
+        hStdIn, handle = win32popen.MakeSpyPipe(1, 0, (dbgIn,))
+        x, hStdOut = win32popen.MakeSpyPipe(None, 1, (dbgOut,))        
+        x, hStdErr = win32popen.MakeSpyPipe(None, 1, (dbgErr,))
+      else:
+        hStdIn, handle = win32popen.CreatePipe(0, 1, 1, 1)
+        hStdOut = None
+        hStdErr = None
+
+    phandle, pid, thandle, tid = win32popen.CreateProcess(command, hStdIn, hStdOut, hStdErr)
+
+    if debug.SHOW_CHILD_PROCESSES:
+      sapi.server.processes.append(debug.Process(command, dbgIn, dbgOut, dbgErr))
+    
+    return _pipe(win32popen.File2FileObject(handle, mode), phandle)
+
   # flush the stdio buffers since we are about to change the FD under them
   sys.stdout.flush()
   sys.stderr.flush()
@@ -35,18 +86,18 @@ def popen(cmd, args, mode, capture_err=1):
     # in the parent
 
     # close the descriptor that we don't need and return the other one.
-    if mode == 'r':
+    if mode.find('r') >= 0:
       os.close(w)
-      return _pipe(os.fdopen(r, 'r'), pid)
+      return _pipe(os.fdopen(r, mode), pid)
     os.close(r)
-    return _pipe(os.fdopen(w, 'w'), pid)
+    return _pipe(os.fdopen(w, mode), pid)
 
   # in the child
 
   # we'll need /dev/null for the discarded I/O
   null = os.open('/dev/null', os.O_RDWR)
 
-  if mode == 'r':
+  if mode.find('r') >= 0:
     # hook stdout/stderr to the "write" channel
     os.dup2(w, 1)
     # "close" stdin; the child shouldn't use it
@@ -83,6 +134,72 @@ def popen(cmd, args, mode, capture_err=1):
   sys.exit(127)
 
 def pipe_cmds(cmds):
+  """Executes a sequence of commands. The output of each command is directed to
+  the input of the next command. A _pipe object is returned for writing to the
+  first command's input. The output of the last command is directed to the
+  standard out. On windows, if sys.stdout is not an inheritable file handle
+  (i.e. it is not possible to direct the standard out of a child process to
+  it), then a separate thread will be spawned to spool output to
+  sys.stdout.write(). In all cases, the pipe_cmds() caller should refrain
+  from writing to the standard out until the last process has terminated.
+  """
+  if sys.platform == "win32":
+  
+    if debug.SHOW_CHILD_PROCESSES:
+      dbgIn = StringIO.StringIO()
+      hStdIn, handle = win32popen.MakeSpyPipe(1, 0, (dbgIn,))
+      
+      i = 0
+      for cmd in cmds:
+        i += 1
+        
+        dbgOut, dbgErr = StringIO.StringIO(), StringIO.StringIO()
+        
+        if i < len(cmds):
+          nextStdIn, hStdOut = win32popen.MakeSpyPipe(1, 1, (dbgOut,))
+          x, hStdErr = win32popen.MakeSpyPipe(None, 1, (dbgErr,))
+        else:
+          ehandle = win32event.CreateEvent(None, 1, 0, None)
+          nextStdIn, hStdOut = win32popen.MakeSpyPipe(None, 1, (dbgOut, sapi.server.getFile()), ehandle)
+          x, hStdErr = win32popen.MakeSpyPipe(None, 1, (dbgErr,))
+
+        command = win32popen.CommandLine(cmd[0], cmd[1:])
+        phandle, pid, thandle, tid = win32popen.CreateProcess(command, hStdIn, hStdOut, None)
+        if debug.SHOW_CHILD_PROCESSES:
+          sapi.server.processes.append(debug.Process(command, dbgIn, dbgOut, dbgErr))
+          
+        dbgIn = dbgOut
+        hStdIn = nextStdIn
+
+    
+    else:  
+  
+      hStdIn, handle = win32popen.CreatePipe(1, 1, 0, 1)
+      spool = None
+  
+      i = 0
+      for cmd in cmds:
+        i += 1
+        if i < len(cmds):
+          nextStdIn, hStdOut = win32popen.CreatePipe(1, 1, 1, 1)
+        else:
+          # very last process
+          nextStdIn = None
+          
+          if sapi.server.inheritableOut:
+            # send child output to standard out
+            hStdOut = win32popen.MakeInheritedHandle(win32popen.FileObject2File(sys.stdout),0)
+            ehandle = None
+          else:
+            ehandle = win32event.CreateEvent(None, 1, 0, None)
+            x, hStdOut = win32popen.MakeSpyPipe(None, 1, (sapi.server.getFile(),), ehandle)            
+  
+        command = win32popen.CommandLine(cmd[0], cmd[1:])
+        phandle, pid, thandle, tid = win32popen.CreateProcess(command, hStdIn, hStdOut, None)
+        hStdIn = nextStdIn
+        
+    return _pipe(win32popen.File2FileObject(handle, 'wb'), phandle, ehandle)
+
   # flush the stdio buffers since we are about to change the FD under them
   sys.stdout.flush()
   sys.stderr.flush()
@@ -163,11 +280,24 @@ def pipe_cmds(cmds):
 class _pipe:
   "Wrapper for a file which can wait() on a child process at close time."
 
-  def __init__(self, file, child_pid):
+  def __init__(self, file, child_pid, done_event = None):
     self.file = file
     self.child_pid = child_pid
+    if sys.platform == "win32":
+      if done_event:
+        self.wait_for = (child_pid, done_event)
+      else:
+        self.wait_for = (child_pid,)
 
   def eof(self):
+    if sys.platform == "win32":
+      r = win32event.WaitForMultipleObjects(self.wait_for, 1, 0)
+      if r == win32event.WAIT_OBJECT_0:
+        self.file.close()
+        self.file = None
+        return win32process.GetExitCodeProcess(self.child_pid)
+      return None
+
     pid, status = os.waitpid(self.child_pid, os.WNOHANG)
     if pid:
       self.file.close()
@@ -179,12 +309,15 @@ class _pipe:
     if self.file:
       self.file.close()
       self.file = None
-      return os.waitpid(self.child_pid, 0)[1]
+      if sys.platform == "win32":
+        win32event.WaitForMultipleObjects(self.wait_for, 1, win32event.INFINITE)
+        return win32process.GetExitCodeProcess(self.child_pid)
+      else:
+        return os.waitpid(self.child_pid, 0)[1]
     return None
 
   def __getattr__(self, name):
     return getattr(self.file, name)
 
   def __del__(self):
-    if self.file:
-      self.close()
+    self.close()
