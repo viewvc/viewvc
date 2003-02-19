f55d42ea8507e8d8d7c24a4e82cde66130cae564
rey4 <rey4@8cb11bc2-c004-0410-86c3-e597b4017df7>
2003-02-19 06:23:32 +0000

    simplified apache handler using execfile()

--- lib/apache.py
+++ lib/apache.py
@@ -1,39 +1,6 @@
-from mod_python import apache, util 
-import imp 
-import os 
-import sys
-import thread
-
-requests = {}
-
-def GetRequest():
-  return requests[thread.get_ident()]
+from mod_python import apache
 
 def handler(req):
-  # based on mod_python.cgihandler
-  
   req.add_common_vars()
-
-  if req.subprocess_env.has_key("script_filename"):
-      dir, file = os.path.split(req.subprocess_env["script_filename"])
-  else:
-      dir, file = os.path.split(req.filename)
-  module_name, ext = os.path.splitext(file)
-
-  try:
-      # we do not search the pythonpath (security reasons)
-      fd, path, desc = imp.find_module(module_name, [dir])
-  except ImportError:
-      raise apache.SERVER_RETURN, apache.HTTP_NOT_FOUND
-  
-  # prevent scripts named query.py and viewcvs.py from shadowing
-  # modules in viewcvs/lib directory
-  module_name += '_page'
-  
-  requests[thread.get_ident()] = req
-  try:
-    imp.load_module(module_name, fd, path, desc)
-  finally:
-    del requests[thread.get_ident()]
-
+  execfile(req.subprocess_env["script_filename"], {}, {'Request': req})
   return apache.OK
