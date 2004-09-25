31fdcf3825b22918049696e74f4d6e5ee1f7927f
rey4 <rey4@8cb11bc2-c004-0410-86c3-e597b4017df7>
2004-09-25 00:32:01 +0000

    Bugfix in query page, stop using old "icons" config variable
    * lib/query.py
      (main)
         stop trying to set "icons" template variable

--- lib/query.py
+++ lib/query.py
@@ -407,7 +407,6 @@ def main(server, viewcvs_link):
       'cfg' : cfg,
       'address' : cfg.general.address,
       'vsn' : viewcvs.__version__,
-      'icons' : cfg.options.icons,
 
       'repository' : server.escape(form_data.repository, 1),
       'branch' : server.escape(form_data.branch, 1),
