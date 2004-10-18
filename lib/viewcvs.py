f686aa71820120ba05182e53bda4c14afd5feb14
cmpilato <cmpilato@users.noreply.github.com>
2004-10-18 16:03:44 +0000

    Address issue #1049046.
    * lib/viewcvs.py
      (_re_rewrite_email): Allow plus signs in email addresses.

--- lib/viewcvs.py
+++ lib/viewcvs.py
@@ -735,7 +735,7 @@ def is_plain_text(mime_type):
   return not mime_type or mime_type == 'text/plain'
 
 _re_rewrite_url = re.compile('((http|https|ftp|file|svn|svn\+ssh)(://[-a-zA-Z0-9%.~:_/]+)([?&]([-a-zA-Z0-9%.~:_]+)=([-a-zA-Z0-9%.~:_])+)*(#([-a-zA-Z0-9%.~:_]+)?)?)')
-_re_rewrite_email = re.compile('([-a-zA-Z0-9_.]+)@(([-a-zA-Z0-9]+\.)+[A-Za-z]{2,4})')
+_re_rewrite_email = re.compile('([-a-zA-Z0-9_.\+]+)@(([-a-zA-Z0-9]+\.)+[A-Za-z]{2,4})')
 def htmlify(html):
   html = cgi.escape(html)
   html = re.sub(_re_rewrite_url, r'<a href="\1">\1</a>', html)
