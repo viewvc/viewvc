3af5af413baad5e72321f3f3d8b1ba8b14948101
rey4 <rey4@8cb11bc2-c004-0410-86c3-e597b4017df7>
2005-02-03 13:03:19 +0000

    Add revision links to annotate view
    * lib/viewcvs.py
      (view_markup, nav_header_data)
        move "view_href," "download_href," "annotate_href" assignments
        into nav_header_data
    
    * templates/annotate.ezt
        add revision links here

--- lib/viewcvs.py
+++ lib/viewcvs.py
@@ -938,6 +938,48 @@ def nav_header_data(request, rev):
   data.update({
     'rev' : rev
   })
+
+  data['view_href'] = request.get_url(view_func=view_markup,
+                                      params={'rev': rev},
+                                      escape=1)
+
+  data['download_href'] = request.get_url(view_func=view_checkout,
+                                          params={'rev': rev},
+                                          escape=1)
+
+  if not is_plain_text(request.mime_type):
+    data['download_text_href'] = \
+      request.get_url(view_func=view_checkout,
+                      params={'content-type': 'text/plain', 'rev': rev},
+                      escape=1)
+  else:
+    data['download_text_href'] = None
+
+  if request.roottype == 'cvs':
+    data['annotate_href'] = request.get_url(view_func=view_annotate,
+                                            params={'annotate': rev},
+                                            escape=1)
+  else:
+    data['annotate_href'] = None
+
+  data['download_href'] = request.get_url(view_func=view_checkout,
+                                          params={'rev': rev},
+                                          escape=1)
+  if not is_plain_text(request.mime_type):
+    data['download_text_href'] = \
+      request.get_url(view_func=view_checkout,
+                      params={'content-type': 'text/plain', 'rev': rev},
+                      escape=1)
+  else:
+    data['download_text_href'] = None
+
+  if request.roottype == 'cvs':
+    data['annotate_href'] = request.get_url(view_func=view_annotate,
+                                            params={'annotate': rev},
+                                            escape=1)
+  else:
+    data['annotate_href'] = None
+
   return data
 
 def retry_read(src, reqlen=CHUNK_SIZE):
@@ -1269,24 +1311,6 @@ def view_markup(request):
     'mime_type' : request.mime_type,
     'log' : None,
     })
-
-  data['download_href'] = request.get_url(view_func=view_checkout,
-                                          params={'rev': rev},
-                                          escape=1)
-  if not is_plain_text(request.mime_type):
-    data['download_text_href'] = \
-      request.get_url(view_func=view_checkout,
-                      params={'content-type': 'text/plain', 'rev': rev},
-                      escape=1)
-  else:
-    data['download_text_href'] = None
-
-  if request.roottype == 'cvs':
-    data['annotate_href'] = request.get_url(view_func=view_annotate,
-                                            params={'annotate': rev},
-                                            escape=1)
-  else:
-    data['annotate_href'] = None
 
   if cfg.options.show_log_in_markup:
     options = {}
