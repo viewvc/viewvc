viewvc/viewvc

commit b578d0147092461062e902852f0dc7c72afcfc2d

Tweak logic used to find installed paths ("lib" directory, configuration file, templates, etc). Specifically, make the following changes:
- Get rid of hardcoded paths in module files ("lib" directory) and
  configuration files (viewcvs.conf and mod_python's .htaccess)

- Allow stub scripts (for asp, cgi, and mod_python) to specify
  configuration files so it's possible to have multiple configurations of
  viewcvs running off a single installation.

- Be more consistent about resolving paths when they aren't hardcoded
  (when ViewCVS is running from a source directory). In particular, try
  not to depend on the working directory. That way it's legal to run
  ./standalone.py instead of bin/standalone.py without getting an
  ImportError.

- Get rid of global cfg variables in viewcvs.py and cvsdb.py. They led to
  all sorts of hacks in other files to pilfer and reset them. They were
  also possible sources of races in multithreaded environments like
  mod_python and asp.

- Rewrite mod_python handler so library paths can be specified inside the
  stub files.

* lib/apache.py
    removed, contained old mod_python handler

* lib/config.py
  (Config.load_config):
    remove sys.argv voodoo, just load the configuration path specified in
    the pathname argument

  (Config.set_defaults):
    use relative path for cvsgraph_conf setting instead of hardcoded
    <VIEWCVS_INSTALL_DIRECTORY> literal

* lib/cvsdb.py
  (CONF_PATHNAME, config, cfg):
    removed, configuration stuff

  (CheckinDatabase.__init__, CheckinDatabase.CreateSQLQueryString):
    add "_row_limit" member instead of using cfg object

  (CreateCheckinDatabase):
    removed, a do-nothing function

  (ConnectDatabaseReadOnly, ConnectDatabase):
    add cfg arguments

  (GetUnrecordedCommitList):
    add db argument

* lib/query.py
  (CONF_PATHAME):
    removed

  (build_commit, run_query, main):
    add cfg arguments, use new viewcvs.get_template function

* lib/viewcvs.py
  (CONF_PATHNAME, cfg, g_install_dir):

lib/config.py
@@ -50,14 +50,7 @@ class Config:
     for section in self._sections:
       setattr(self, section, _sub_config())
 
-  def load_config(self, fname, vhost=None):
-    if hasattr(sys, 'argv'):
-      this_dir = os.path.dirname(sys.argv[0])
-    else:
-      this_dir = ''
-    pathname = os.path.join(this_dir, fname)
-    self.base = os.path.dirname(pathname)
-
+  def load_config(self, pathname, vhost=None):
     self.conf_path = os.path.isfile(pathname) and pathname or None
 
     parser = ConfigParser.ConfigParser()
@@ -216,7 +209,7 @@ class Config:
     self.options.allow_tar = 0
     self.options.use_cvsgraph = 0
     self.options.cvsgraph_path = ''
-    self.options.cvsgraph_conf = os.path.join(r"<VIEWCVS_INSTALL_DIRECTORY>", "cvsgraph.conf")
+    self.options.cvsgraph_conf = "cvsgraph.conf"
     self.options.use_re_search = 0
     self.options.use_pagesize = 0
     self.options.use_localtime = 0

bin/asp/query.asp
@@ -15,12 +15,12 @@
 #
 # -----------------------------------------------------------------------
 #
-# query.cgi: View CVS commit database by web browser
+# query.asp: View CVS commit database by web browser
 #
 # -----------------------------------------------------------------------
 #
 # This is a teeny stub to launch the main ViewCVS app. It checks the load
-# average, then loads the (precompiled) viewcvs.py file and runs it.
+# average, then loads the (precompiled) query.py file and runs it.
 #
 # -----------------------------------------------------------------------
 #
@@ -34,6 +34,7 @@
 #
 
 LIBRARY_DIR = None
+CONF_PATHNAME = None
 
 #########################################################################
 #
@@ -45,17 +46,17 @@ import sys
 if LIBRARY_DIR:
   if not LIBRARY_DIR in sys.path:
     sys.path.insert(0, LIBRARY_DIR)
-else:
-  sys.path[:0] = ['../lib']	# any other places to look?
 
 #########################################################################
 
 import sapi
+import viewcvs
 import query
 
-s = sapi.AspServer(Server, Request, Response, Application)
+server = sapi.AspServer(Server, Request, Response, Application)
 try:
-  query.main(s, 'viewcvs.asp')
+  cfg = viewcvs.load_config(CONF_PATHNAME, server)
+  query.main(server, cfg, "viewcvs.asp")
 finally:
   s.close()
 

bin/asp/viewcvs.asp
@@ -34,6 +34,7 @@
 #
 
 LIBRARY_DIR = None
+CONF_PATHNAME = None
 
 #########################################################################
 #
@@ -45,8 +46,6 @@ import sys
 if LIBRARY_DIR:
   if not LIBRARY_DIR in sys.path:
     sys.path.insert(0, LIBRARY_DIR)
-else:
-  sys.path[:0] = ['../lib']	# any other places to look?
 
 #########################################################################
 
@@ -58,9 +57,10 @@ else:
 import sapi
 import viewcvs
 
-s = sapi.AspServer(Server, Request, Response, Application)
+server = sapi.AspServer(Server, Request, Response, Application)
 try:
-  viewcvs.main(s)  
+  cfg = viewcvs.load_config(CONF_PATHNAME, server)
+  viewcvs.main(server, cfg)
 finally:
   s.close()
 

bin/cgi/query.cgi
@@ -32,6 +32,7 @@
 #
 
 LIBRARY_DIR = None
+CONF_PATHNAME = None
 
 #########################################################################
 #
@@ -39,15 +40,20 @@ LIBRARY_DIR = None
 #
 
 import sys
+import os
 
 if LIBRARY_DIR:
   sys.path.insert(0, LIBRARY_DIR)
 else:
-  sys.path[:0] = ['../../lib']	# any other places to look?
+  sys.path.insert(0, os.path.abspath(os.path.join(sys.argv[0],
+                                                  "../../../lib")))
 
 #########################################################################
 
 import sapi
+import viewcvs
 import query
 
-query.main(sapi.CgiServer(), 'viewcvs.cgi')
+server = sapi.CgiServer()
+cfg = viewcvs.load_config(CONF_PATHNAME, server)
+query.main(server, cfg, "viewcvs.cgi")

bin/cgi/viewcvs.cgi
@@ -32,6 +32,7 @@
 #
 
 LIBRARY_DIR = None
+CONF_PATHNAME = None
 
 #########################################################################
 #
@@ -39,11 +40,13 @@ LIBRARY_DIR = None
 #
 
 import sys
+import os
 
 if LIBRARY_DIR:
   sys.path.insert(0, LIBRARY_DIR)
 else:
-  sys.path[:0] = ['../../lib']	# any other places to look?
+  sys.path.insert(0, os.path.abspath(os.path.join(sys.argv[0],
+                                                  "../../../lib")))
 
 #########################################################################
 
@@ -55,4 +58,6 @@ else:
 import sapi
 import viewcvs
 
-viewcvs.main(sapi.CgiServer())
+server = sapi.CgiServer()
+cfg = viewcvs.load_config(CONF_PATHNAME, server)
+viewcvs.main(server, cfg)

bin/cvsdbadmin
@@ -32,24 +32,26 @@ CONF_PATHNAME = None
 
 # Adjust sys.path to include our library directory
 import sys
+import os
 
 if LIBRARY_DIR:
   sys.path.insert(0, LIBRARY_DIR)
 else:
-  sys.path[:0] = ['../lib']	# any other places to look?
+  sys.path.insert(0, os.path.abspath(os.path.join(sys.argv[0], "../../lib")))
 
 #########################################################################
   
 import os
 import string
 import cvsdb
+import viewcvs
 import vclib.bincvs
 
 
 def UpdateFile(db, repository, path, update):
     try:
         if update:
-            commit_list = cvsdb.GetUnrecordedCommitList(repository, path)
+            commit_list = cvsdb.GetUnrecordedCommitList(repository, path, db)
         else:
             commit_list = cvsdb.GetCommitListFromRCSFile(repository, path)
     except cvsdb.error, e:
@@ -119,10 +121,10 @@ if __name__ == '__main__':
     ## run command
     try:
       ## connect to the database we are updating
-      db = cvsdb.ConnectDatabase()
+      cfg = viewcvs.load_config(CONF_PATHNAME)
+      db = cvsdb.ConnectDatabase(cfg)
 
-      repository = vclib.bincvs.BinCVSRepository \
-        (None, path, cvsdb.cfg.general)
+      repository = vclib.bincvs.BinCVSRepository(None, path, cfg.general)
 
       RecurseUpdate(db, repository, [], update)
 

bin/loginfo-handler
@@ -31,11 +31,12 @@ CONF_PATHNAME = None
 
 # Adjust sys.path to include our library directory
 import sys
+import os
 
 if LIBRARY_DIR:
   sys.path.insert(0, LIBRARY_DIR)
 else:
-  sys.path[:0] = ['../lib']	# any other places to look?
+  sys.path.insert(0, os.path.abspath(os.path.join(sys.argv[0], "../../lib")))
 
 #########################################################################
 
@@ -44,7 +45,7 @@ import string
 import getopt
 import re
 import cvsdb
-import config
+import viewcvs
 import vclib.bincvs
 
 DEBUG_FLAG = 0
@@ -197,11 +198,9 @@ def NextFile(s, pos = 0):
   return ret or None, i
 
 def ProcessLoginfo(rootpath, directory, files):
-    ## XXX This is a somewhat dirty hack:
-    ## cvsdb already loads the configuration file and provides a cfg
-    ## instance.  Pick it from there in order to be able to pass it down
-    ## to rlog (see below).
-    repository = vclib.bincvs.BinCVSRepository(None, rootpath, cvsdb.cfg.general)
+    cfg = viewcvs.load_config(CONF_PATHNAME)
+    db = cvsdb.ConnectDatabase(cfg)
+    repository = vclib.bincvs.BinCVSRepository(None, rootpath, cfg.general)
 
     # split up the directory components
     dirpath = filter(None, string.split(os.path.normpath(directory), os.sep))
@@ -216,7 +215,7 @@ def ProcessLoginfo(rootpath, directory, files):
         ##      So instead, we rlog everything in the removed file, and
         ##      add any commits not already in the database
         if new_version == 'NONE':
-            commits = cvsdb.GetUnrecordedCommitList(repository, filepath)
+            commits = cvsdb.GetUnrecordedCommitList(repository, filepath, db)
         else:
             commits = cvsdb.GetCommitListFromRCSFile(repository, filepath,
                                                      new_version)
@@ -224,7 +223,6 @@ def ProcessLoginfo(rootpath, directory, files):
         commit_list.extend(commits)
 
     ## add to the database
-    db = cvsdb.ConnectDatabase()
     db.AddCommitList(commit_list)
 
 

bin/mod_python/.htaccess
@@ -1,4 +1,3 @@
 AddHandler python-program .py
-PythonPath "[r'<VIEWCVS_APACHE_LIBRARY_DIRECTORY>']+sys.path"
-PythonHandler apache
+PythonHandler handler
 PythonDebug On

commit 228271ebe8969c26c913cb889647175cd0c94a8a

Fix exception caused by r1173 when kv files are used. This is issue 170 reported by cmpilato.
* lib/viewcvs.py:
  (Config.load_config):
    re-add line from before r1173 that assigns "base" member

lib/config.py
@@ -52,6 +52,7 @@ class Config:
 
   def load_config(self, pathname, vhost=None):
     self.conf_path = os.path.isfile(pathname) and pathname or None
+    self.base = os.path.dirname(pathname)
 
     parser = ConfigParser.ConfigParser()
     parser.read(pathname)