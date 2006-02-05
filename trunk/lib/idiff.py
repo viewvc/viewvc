d13c111ce606bb33125261d70acbd240652c44ab
rey4 <rey4@8cb11bc2-c004-0410-86c3-e597b4017df7>
2006-02-05 17:32:44 +0000

    Implement trac-style unified diffs (with line numbers along the side and intraline changes highlighted). This feature requires Python 2.2 and is disabled by default.
    * templates/diff.ezt
        add table for highlighted unified diffs
    
    * lib/idiff.py
      (unified, _trim_context, _differ_split): new
    
    * lib/viewcvs.py
      (view_diffs): set new "unified" template var on diff page

--- trunk/lib/idiff.py
+++ trunk/lib/idiff.py
@@ -68,6 +68,115 @@ def _mdiff_split(flag, (line_number, text)):
 
   return _item(segments=segments, line_number=line_number)  
 
+def unified(fromlines, tolines, context):
+  """Generate unified diff"""
+
+  diff = difflib.Differ().compare(fromlines, tolines)
+  lastrow = None
+
+  for row in _trim_context(diff, context):
+    if row[0].startswith("? "):
+      yield _differ_split(lastrow, row[0])
+      lastrow = None
+    else:
+      if lastrow:
+        yield _differ_split(lastrow, None)
+      lastrow = row
+
+  if lastrow:
+    yield _differ_split(lastrow, None)
+
+def _trim_context(lines, context_size):
+  """Trim context lines that don't surround changes from Differ results
+
+  yields (line, leftnum, rightnum, gap) tuples"""
+
+  # circular buffer to hold context lines
+  context_buffer = [None] * (context_size or 0)
+  context_start = context_len = 0
+
+  # number of context lines left to print after encountering a change
+  context_owed = 0
+
+  # current line numbers
+  leftnum = rightnum = 0
+
+  # whether context lines have been dropped
+  gap = False
+
+  for line in lines:
+    row = save = None
+
+    if line.startswith("- "):
+      leftnum = leftnum + 1
+      row = line, leftnum, None
+      context_owed = context_size
+
+    elif line.startswith("+ "):
+      rightnum = rightnum + 1
+      row = line, None, rightnum
+      context_owed = context_size
+
+    else:
+      if line.startswith("  "):
+        leftnum = leftnum = leftnum + 1
+        rightnum = rightnum = rightnum + 1
+        if context_owed > 0:
+          context_owed = context_owed - 1
+        elif context_size is not None:
+          save = True
+
+      row = line, leftnum, rightnum
+
+    if save:
+      # don't yield row right away, store it in buffer
+      context_buffer[(context_start + context_len) % context_size] = row
+      if context_len == context_size:
+        context_start = (context_start + 1) % context_size
+        gap = True
+      else:
+        context_len = context_len + 1
+    else:
+      # yield row, but first drain stuff in buffer
+      context_len == context_size
+      while context_len:
+        yield context_buffer[context_start] + (gap,)
+        gap = False
+        context_start = (context_start + 1) % context_size
+        context_len = context_len - 1
+      yield row + (gap,)
+      gap = False
+
+_re_differ = re.compile(r"[+-^]+")
+
+def _differ_split(row, guide):
+  """Break row into segments using guide line"""
+  line, left_number, right_number, gap = row
+
+  if left_number and right_number:
+    type = "" 
+  elif left_number:
+    type = "remove"
+  elif right_number:
+    type = "add"
+
+  segments = []  
+  pos = 2
+
+  if guide:
+    assert guide.startswith("? ")
+
+    for m in _re_differ.finditer(guide, pos):
+      if m.start() > pos:
+        segments.append(_item(text=line[pos:m.start()], type=None))
+      segments.append(_item(text=line[m.start():m.end()], type="change"))
+      pos = m.end()
+
+  segments.append(_item(text=line[pos:], type=None))
+
+  return _item(gap=ezt.boolean(gap), type=type, segments=segments,
+               left_number=left_number, right_number=right_number)
+
 class _item:
   def __init__(self, **kwargs):
     vars(self).update(**kwargs)
