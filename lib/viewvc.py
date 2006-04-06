f504b48c1c7d778fb7df71735aaace3f3f41c090
C. Michael Pilato <cmpilato@red-bean.com>
2006-04-06 19:56:43 +0000

    Tag 1.0.0-rc1.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.1-dev'
+__version__ = '1.0.0-rc1'
 
 # this comes from our library; measure the startup time
 import debug
