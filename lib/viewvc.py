c237e5c0151361b92ce30b5471a6dd913cc11376
cmpilato <cmpilato@users.noreply.github.com>
2006-07-21 01:04:12 +0000

    * lib/viewvc.py   (__version__): Bump to 1.0.2-dev.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.0.1-dev'
+__version__ = '1.0.2-dev'
 
 # this comes from our library; measure the startup time
 import debug
