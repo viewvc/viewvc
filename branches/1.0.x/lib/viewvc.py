e04489d1673d060c334f2fd07f4ef70b1f2052d0
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2006-10-13 20:31:49 +0000

    * CHANGES   Finalize 1.0.3 changes list.  Begin a new section for 1.0.4.
    * lib/viewvc.py
      (__version__): Bump to 1.0.4-dev.

--- branches/1.0.x/lib/viewvc.py
+++ branches/1.0.x/lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.0.3-dev'
+__version__ = '1.0.4-dev'
 
 # this comes from our library; measure the startup time
 import debug
