05f98cb81e20e902f4cd788f82bf9e673fe5bf34
cmpilato <cmpilato@users.noreply.github.com>
2006-11-09 21:09:05 +0000

    * lib/vclib/svn_ra/__init__.py   Purge the svn_ra module of unnecessary pools.

--- lib/vclib/svn_ra/__init__.py
+++ lib/vclib/svn_ra/__init__.py
@@ -38,15 +38,13 @@ def _rev2optrev(rev):
 
 
 def date_from_rev(svnrepos, rev):
-  datestr = ra.svn_ra_rev_prop(svnrepos.ra_session, rev,
-                               'svn:date', svnrepos.pool)
-  return _datestr_to_date(datestr, svnrepos.pool)
+  return _datestr_to_date(ra.svn_ra_rev_prop(svnrepos.ra_session, rev,
+                                             core.SVN_PROP_REVISION_DATE))
 
 
 def get_location(svnrepos, path, rev, old_rev):
   try:
-    results = ra.get_locations(svnrepos.ra_session, path, rev,
-                               [old_rev], svnrepos.pool)
+    results = ra.get_locations(svnrepos.ra_session, path, rev, [old_rev])
   except core.SubversionException, e:
     if e.apr_err == core.SVN_ERR_FS_NOT_FOUND:
       raise vclib.ItemNotFound(path)
@@ -83,7 +81,6 @@ def last_rev(svnrepos, path, peg_revision, limit_revision=None):
     path = get_location(svnrepos, path, peg_revision, limit_revision)
     return limit_revision, path
   else:
-    ### Warning: this is *not* an example of good pool usage.
     direction = 1
     while peg_revision != limit_revision:
       mid = (peg_revision + 1 + limit_revision) / 2
@@ -97,11 +94,9 @@ def last_rev(svnrepos, path, peg_revision, limit_revision=None):
 
 
 def created_rev(svnrepos, full_name, rev):
-  kind = ra.svn_ra_check_path(svnrepos.ra_session, full_name, rev,
-                              svnrepos.pool)
+  kind = ra.svn_ra_check_path(svnrepos.ra_session, full_name, rev)
   if kind == core.svn_node_dir:
-    props = ra.svn_ra_get_dir(svnrepos.ra_session, full_name,
-                              rev, svnrepos.pool)
+    props = ra.svn_ra_get_dir(svnrepos.ra_session, full_name, rev)
     return int(props[core.SVN_PROP_ENTRY_COMMITTED_REV])
   return core.SVN_INVALID_REVNUM
 
@@ -148,18 +143,17 @@ class LastHistoryCollector:
     return self.revision, self.author, self.date, self.message, self.changes
 
 
-def _get_rev_details(svnrepos, rev, pool):
+def _get_rev_details(svnrepos, rev):
   lhc = LastHistoryCollector()
   client.svn_client_log([svnrepos.rootpath],
                         _rev2optrev(rev), _rev2optrev(rev),
-                        1, 0, lhc.add_history, svnrepos.ctx, pool)
+                        1, 0, lhc.add_history, svnrepos.ctx)
   return lhc.get_history()
 
   
 def get_revision_info(svnrepos, rev):
-  rev, author, date, log, changes = \
-       _get_rev_details(svnrepos, rev, svnrepos.pool)
-  return _datestr_to_date(date, svnrepos.pool), author, log, changes
+  rev, author, date, log, changes = _get_rev_details(svnrepos, rev)
+  return _datestr_to_date(date), author, log, changes
 
 
 class LogCollector:
@@ -192,8 +186,7 @@ class LogCollector:
           if change.copyfrom_path:
             this_path = change.copyfrom_path + self.path[len(changed_path):]
     if self.show_all_logs or this_path:
-      date = _datestr_to_date(date, pool)
-      entry = Revision(revision, date, author, message, None,
+      entry = Revision(revision, _datestr_to_date(date), author, message, None,
                        self.path[1:], None, None)
       self.logs.append(entry)
     if this_path:
@@ -202,35 +195,31 @@ class LogCollector:
 
 def get_logs(svnrepos, full_name, rev, files):
   dirents = svnrepos._get_dirents(full_name, rev)
-  subpool = core.svn_pool_create(svnrepos.pool)
   rev_info_cache = { }
   for file in files:
-    core.svn_pool_clear(subpool)
     entry = dirents[file.name]
     if rev_info_cache.has_key(entry.created_rev):
       rev, author, date, log = rev_info_cache[entry.created_rev]
     else:
       ### i think this needs some get_last_history action to be accurate
       rev, author, date, log, changes = \
-           _get_rev_details(svnrepos, entry.created_rev, subpool)
+           _get_rev_details(svnrepos, entry.created_rev)
       rev_info_cache[entry.created_rev] = rev, author, date, log
     file.rev = rev
     file.author = author
-    file.date = _datestr_to_date(date, subpool)
+    file.date = _datestr_to_date(date)
     file.log = log
     file.size = entry.size
-  core.svn_pool_destroy(subpool)    
 
 def get_youngest_revision(svnrepos):
   return svnrepos.youngest
 
-def temp_checkout(svnrepos, path, rev, pool):
+def temp_checkout(svnrepos, path, rev):
   """Check out file revision to temporary file"""
-  temp = tempfile.mktemp()
-  stream = core.svn_stream_from_aprfile(temp, pool)
+  stream = core.svn_stream_from_aprfile(tempfile.mktemp())
   url = svnrepos.rootpath + (path and '/' + path)
   client.svn_client_cat(core.Stream(stream), url, _rev2optrev(rev),
-                        svnrepos.ctx, pool)
+                        svnrepos.ctx)
   core.svn_stream_close(stream)
   return temp
 
@@ -268,13 +257,9 @@ class SelfCleanFP:
 
 class SubversionRepository(vclib.Repository):
   def __init__(self, name, rootpath, utilities):
-    # Init the client app
-    core.apr_initialize()
-    pool = core.svn_pool_create(None)
     core.svn_config_ensure(None, pool)
 
     # Start populating our members
-    self.pool = pool
     self.name = name
     self.rootpath = rootpath
     self.diff_cmd = utilities.diff or 'diff'
@@ -282,26 +267,22 @@ class SubversionRepository(vclib.Repository):
     # Setup the client context baton, complete with non-prompting authstuffs.
     ctx = client.svn_client_ctx_t()
     providers = []
-    providers.append(client.svn_client_get_simple_provider(pool))
-    providers.append(client.svn_client_get_username_provider(pool))
-    providers.append(client.svn_client_get_ssl_server_trust_file_provider(pool))
-    providers.append(client.svn_client_get_ssl_client_cert_file_provider(pool))
-    providers.append(client.svn_client_get_ssl_client_cert_pw_file_provider(pool))
-    ctx.auth_baton = core.svn_auth_open(providers, pool)
-    ctx.config = core.svn_config_get_config(None, pool)
+    providers.append(client.svn_client_get_simple_provider())
+    providers.append(client.svn_client_get_username_provider())
+    providers.append(client.svn_client_get_ssl_server_trust_file_provider())
+    providers.append(client.svn_client_get_ssl_client_cert_file_provider())
+    providers.append(client.svn_client_get_ssl_client_cert_pw_file_provider())
+    ctx.auth_baton = core.svn_auth_open(providers)
+    ctx.config = core.svn_config_get_config(None)
     self.ctx = ctx
 
     ra_callbacks = ra.svn_ra_callbacks_t()
     ra_callbacks.auth_baton = ctx.auth_baton
     self.ra_session = ra.svn_ra_open(self.rootpath, ra_callbacks, None,
-                                     ctx.config, pool)
-    self.youngest = ra.svn_ra_get_latest_revnum(self.ra_session, pool)
+                                     ctx.config)
+    self.youngest = ra.svn_ra_get_latest_revnum(self.ra_session)
     self._dirent_cache = { }
 
-  def __del__(self):
-    core.svn_pool_destroy(self.pool)
-    core.apr_terminate()
-    
   def itemtype(self, path_parts, rev):
     path = self._getpath(path_parts[:-1])
     rev = self._getrev(rev)
@@ -323,10 +304,9 @@ class SubversionRepository(vclib.Repository):
     if len(path_parts):
       url = self.rootpath + '/' + self._getpath(path_parts)
     tmp_file = tempfile.mktemp()
-    stream = core.svn_stream_from_aprfile(tmp_file, self.pool)
+    stream = core.svn_stream_from_aprfile(tmp_file)
     ### rev here should be the last history revision of the URL
-    client.svn_client_cat(core.Stream(stream), url,
-                          _rev2optrev(rev), self.ctx, self.pool)
+    client.svn_client_cat(core.Stream(stream), url, _rev2optrev(rev), self.ctx)
     core.svn_stream_close(stream)
     return SelfCleanFP(tmp_file), rev
 
@@ -360,8 +340,7 @@ class SubversionRepository(vclib.Repository):
 
     cross_copies = options.get('svn_cross_copies', 0)
     client.svn_client_log([dir_url], _rev2optrev(rev), _rev2optrev(1),
-                          1, not cross_copies, lc.add_log,
-                          self.ctx, self.pool)
+                          1, not cross_copies, lc.add_log, self.ctx)
     revs = lc.logs
     revs.sort()
     prev = None
@@ -387,7 +366,7 @@ class SubversionRepository(vclib.Repository):
                                          author, None))
       
     client.svn_client_blame(url, _rev2optrev(1), _rev2optrev(rev),
-                            _blame_cb, self.ctx, self.pool)
+                            _blame_cb, self.ctx)
 
     return blame_data, rev
     
@@ -399,8 +378,8 @@ class SubversionRepository(vclib.Repository):
     args = vclib._diff_args(type, options)
 
     try:
-      temp1 = temp_checkout(self, p1, r1, self.pool)
-      temp2 = temp_checkout(self, p2, r2, self.pool)
+      temp1 = temp_checkout(self, p1, r1)
+      temp2 = temp_checkout(self, p2, r2)
       info1 = p1, date_from_rev(self, r1), r1
       info2 = p2, date_from_rev(self, r2), r2
       return vclib._diff_fp(temp1, temp2, info1, info2, self.diff_cmd, args)
@@ -433,7 +412,6 @@ class SubversionRepository(vclib.Repository):
     dirents = self._dirent_cache.get(key)
     if dirents:
       return dirents
-    dirents = client.svn_client_ls(dir_url, _rev2optrev(rev), 0,
-                                   self.ctx, self.pool)
+    dirents = client.svn_client_ls(dir_url, _rev2optrev(rev), 0, self.ctx)
     self._dirent_cache[key] = dirents
     return dirents
