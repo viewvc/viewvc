889d27d2e17f4817953405af48e72b576de76d17
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2007-04-17 03:50:20 +0000

    Merge from trunk r1612, and make similar additional tweaks for this branch.
    * lib/config.py
      (Config.load_config): Stash a reference to the primary
        ConfigParser() object.
      (Config.overlay_root_options, Config.get_authorizer_params): Use the
        stashed ConfigParser() reference instead of re-parsing the
        configuration file.

--- branches/authz-dev/lib/config.py
+++ branches/authz-dev/lib/config.py
@@ -50,19 +50,18 @@ class Config:
   def load_config(self, pathname, vhost=None, rootname=None):
     self.conf_path = os.path.isfile(pathname) and pathname or None
     self.base = os.path.dirname(pathname)
-    
-    parser = ConfigParser.ConfigParser()
-    parser.read(self.conf_path or [])
+    self.parser = ConfigParser.ConfigParser()
+    self.parser.read(self.conf_path or [])
 
     for section in self._sections:
-      if parser.has_section(section):
-        self._process_section(parser, section, section)
+      if self.parser.has_section(section):
+        self._process_section(self.parser, section, section)
 
-    if vhost and parser.has_section('vhosts'):
-      self._process_vhost(parser, vhost)
+    if vhost and self.parser.has_section('vhosts'):
+      self._process_vhost(self.parser, vhost)
 
     if rootname:
-      self._process_root_options(parser, rootname)
+      self._process_root_options(self.parser, rootname)
 
   def load_kv_files(self, language):
     kv = _sub_config()
@@ -162,21 +161,20 @@ class Config:
     "Overly per-root options atop the existing option set."
     if not self.conf_path:
       return
-    parser = ConfigParser.ConfigParser()
-    parser.read(self.conf_path or [])
-    self._process_root_options(parser, rootname)
+    self._process_root_options(self.parser, rootname)
 
   def get_authorizer_params(self, authorizer, rootname=None):
-    parser = ConfigParser.ConfigParser()
-    parser.read(self.conf_path or [])
+    if not self.conf_path:
+      return {}
+    
     params = {}
     authz_section = 'authz-%s' % (authorizer)
     if rootname:
       root_authz_section = 'root-%s/authz-%s' % (rootname, authorizer)
-    for section in parser.sections():
+    for section in self.parser.sections():
       if section == authz_section \
          or (rootname and section == root_authz_section):
-        params.update(parser.items(section))
+        params.update(self.parser.items(section))
     return params
   
   def set_defaults(self):
