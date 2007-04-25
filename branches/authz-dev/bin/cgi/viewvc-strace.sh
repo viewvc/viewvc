060de7c32e48747a5ac82aecb0d2e460590e2ae6
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2007-04-25 20:09:50 +0000

    Remove authz-dev branch, whose contents now live on trunk.

--- branches/authz-dev/bin/cgi/viewvc-strace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Set this script up with something like:
-#
-#   ScriptAlias /viewvc-strace /home/gstein/src/viewvc/cgi/viewvc-strace.sh
-#
-thisdir="`dirname $0`"
-exec strace -q -r -o /tmp/v-strace.log "${thisdir}/viewvc.cgi"
