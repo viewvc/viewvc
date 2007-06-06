de50ab2127c4871a9e0868bf02edd19252a41f17
cmpilato <cmpilato@users.noreply.github.com>
2007-06-06 20:32:17 +0000

    The disconnected query interface throws all the root-centric handling convention of ViewVC into a tizzy, so rework the way the auth handling stuff works there.  We'll get root-specific handling of 'forbidden' out of this as a result.
    * lib/query.py
      (is_forbidden): New.
      (build_commit): Lose the 'auth' parameter, and call is_forbidden to
        determine if a given result may be displayed.
      (run_query): No longer grab an Authorizer object.  Update calls to
        build_commit().

--- lib/query.py
+++ lib/query.py
@@ -28,7 +28,7 @@ import viewvc
 import ezt
 import debug
 import urllib
-import vcauth
+import fnmatch
 
 class FormData:
     def __init__(self, form):
@@ -274,7 +274,21 @@ def prev_rev(rev):
         r = r[:-2]
     return string.join(r, '.')
 
-def build_commit(server, cfg, auth, desc, files, cvsroots, viewvc_link):
+def is_forbidden(cfg, cvsroot_name, module):
+    auth_params = cfg.get_authorizer_params('forbidden', cvsroot_name)
+    forbidden = auth_params.get('forbidden', '')
+    forbidden = map(string.strip, filter(None, string.split(forbidden, ',')))
+    default = 0
+    for pat in forbidden:
+        if pat[0] == '!':
+            default = 1
+            if fnmatch.fnmatchcase(module, pat[1:]):
+                return 0
+        elif fnmatch.fnmatchcase(module, pat):
+            return 1
+    return default
+    
+def build_commit(server, cfg, desc, files, cvsroots, viewvc_link):
     ob = _item(num_files=len(files), files=[])
     
     if desc:
@@ -283,10 +297,19 @@ def build_commit(server, cfg, auth, desc, files, cvsroots, viewvc_link):
         ob.log = '&nbsp;'
 
     for commit in files:
-        dir_parts = filter(None, string.split(commit.GetDirectory(), '/'))
-        if dir_parts \
-               and ((dir_parts[0] == 'CVSROOT' and cfg.options.hide_cvsroot) \
-                    or auth.check_path_access(dir_parts)):
+        repository = commit.GetRepository()
+        directory = commit.GetDirectory()
+        cvsroot_name = cvsroots.get(repository)
+
+        ## find the module name (if any)
+        try:
+            module = filter(None, string.split(directory, '/'))[0]
+        except IndexError:
+            module = None
+
+        ## skip commits we aren't supposed to show
+        if module and ((module == 'CVSROOT' and cfg.options.hide_cvsroot) \
+                       or is_forbidden(cfg, cvsroot_name, module)):
             continue
 
         ctime = commit.GetTime()
@@ -297,13 +320,13 @@ def build_commit(server, cfg, auth, desc, files, cvsroots, viewvc_link):
             ctime = time.strftime("%y/%m/%d %H:%M %Z", time.localtime(ctime))
           else:
             ctime = time.strftime("%y/%m/%d %H:%M", time.gmtime(ctime)) \
-                  + ' UTC'
+                    + ' UTC'
         
         ## make the file link
-        repository = commit.GetRepository()
-        directory = commit.GetDirectory()
-        file = (directory and directory + "/") + commit.GetFile()
-        cvsroot_name = cvsroots.get(repository)
+        try:
+            file = (directory and directory + "/") + commit.GetFile()
+        except:
+            raise Exception, str([directory, commit.GetFile()])
 
         ## if we couldn't find the cvsroot path configured in the 
         ## viewvc.conf file, then don't make the link
@@ -335,10 +358,6 @@ def build_commit(server, cfg, auth, desc, files, cvsroots, viewvc_link):
     return ob
 
 def run_query(server, cfg, form_data, viewvc_link):
-    auth_params = cfg.get_authorizer_params('forbidden')
-    auth = vcauth.forbidden.ViewVCAuthorizer(None, None, None, None,
-                                             auth_params)
-
     query = form_to_cvsdb_query(form_data)
     db = cvsdb.ConnectDatabaseReadOnly(cfg)
     db.RunQuery(query)
@@ -361,14 +380,14 @@ def run_query(server, cfg, form_data, viewvc_link):
             files.append(commit)
             continue
 
-        commits.append(build_commit(server, cfg, auth, current_desc, files,
+        commits.append(build_commit(server, cfg, current_desc, files,
                                     cvsroots, viewvc_link))
 
         files = [ commit ]
         current_desc = desc
 
     ## add the last file group to the commit list
-    commits.append(build_commit(server, cfg, auth, current_desc, files,
+    commits.append(build_commit(server, cfg, current_desc, files,
                                 cvsroots, viewvc_link))
 
     return commits
