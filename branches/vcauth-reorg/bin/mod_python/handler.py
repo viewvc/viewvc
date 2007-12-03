e2038142c2a2c8c478abf98ac84f525b781d84df
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2007-12-03 16:01:04 +0000

    Create a new 'vcauth-reorg' branch/playground.

---
+++ branches/vcauth-reorg/bin/mod_python/handler.py
@@ -0,0 +1,31 @@
+# -*-python-*-
+#
+# Copyright (C) 1999-2006 The ViewCVS Group. All Rights Reserved.
+#
+# By using this file, you agree to the terms and conditions set forth in
+# the LICENSE.html file which can be found at the top level of the ViewVC
+# distribution or at http://viewvc.org/license-1.html.
+#
+# For more information, visit http://viewvc.org/
+#
+# -----------------------------------------------------------------------
+#
+# Mod_Python handler based on mod_python.publisher
+#
+# -----------------------------------------------------------------------
+
+from mod_python import apache
+import os.path
+
+def handler(req):
+  path, module_name = os.path.split(req.filename)
+  module_name, module_ext = os.path.splitext(module_name)
+  try:
+    module = apache.import_module(module_name, path=[path])
+  except ImportError:
+    raise apache.SERVER_RETURN, apache.HTTP_NOT_FOUND
+
+  req.add_common_vars()
+  module.index(req)
+
+  return apache.OK
