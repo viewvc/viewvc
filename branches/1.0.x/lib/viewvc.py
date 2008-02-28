89970dae53215c9863d923814481a7e5392d3d28
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2008-02-28 15:39:54 +0000

    Ahh ... begin the cycle anew ...

--- branches/1.0.x/lib/viewvc.py
+++ branches/1.0.x/lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.0.5-dev'
+__version__ = '1.0.6-dev'
 
 # this comes from our library; measure the startup time
 import debug
