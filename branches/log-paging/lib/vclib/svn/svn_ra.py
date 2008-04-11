3f315fa79ad2165634f20aecedcfb3255e31512c
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2008-04-11 20:31:45 +0000

    Merge the bugfix in r1851 (which see) from trunk.

--- branches/log-paging/lib/vclib/svn/svn_ra.py
+++ branches/log-paging/lib/vclib/svn/svn_ra.py
@@ -311,8 +311,7 @@ class RemoteSubversionRepository(vclib.Repository):
   def itemprops(self, path_parts, rev):
     path = self._getpath(path_parts)
     rev = self._getrev(rev)
-    url = self.rootpath + (path and '/' + path)
-    stream, fetched_rev, props = ra.svn_ra_get_dir(self.ra_session, url, rev)
+    stream, fetched_rev, props = ra.svn_ra_get_dir(self.ra_session, path, rev)
     return props
   
   def annotate(self, path_parts, rev):
