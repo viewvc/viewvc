615a4913773477dcc51f914db22cdb215ec09445
C. Michael Pilato <cmpilato@red-bean.com>
2008-11-04 21:15:53 +0000

    Tag the 1.1.0-beta1 release.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.2-dev'
+__version__ = '1.1.0-beta1'
 
 # this comes from our library; measure the startup time
 import debug
