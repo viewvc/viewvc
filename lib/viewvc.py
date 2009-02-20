2a1fbea44a22e0d5a8fcc8b69625b5b5b7d727f4
cmpilato <cmpilato@users.noreply.github.com>
2009-02-20 15:31:27 +0000

    Merge from trunk r2090, whose log message read thusly:
       ViewVC doesn't "do" directory entry sorting by revision for CVS, so
       don't imply that it does in the UI.
       
       * lib/viewvc.py
         (view_directory): Don't provide sortby_rev_href to the template.
       
       * templates/directory.ezt
         Only offer sort links when those links are provided by ViewVC.
       
       * templates/dir_new.ezt
         Only offer sort links when those links are provided by ViewVC.  Use
         date-based sorting if rev-based sorting isn't available.
    
    Also:
    
    * CHANGES
      Note this change.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -1727,6 +1727,9 @@ def view_directory(request):
     data['sortby_%s_href' % sortby] = request.get_url(params={'sortdir':
                                                               revsortdir},
                                                       escape=1)
+  # CVS doesn't support sorting by rev
+  if request.roottype == "cvs":
+    data['sortby_rev_href'] = None
 
   # set cvs-specific fields
   if request.roottype == 'cvs':
