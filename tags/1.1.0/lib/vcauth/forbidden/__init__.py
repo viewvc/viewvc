e13b08f811d5cb701d4d167dfdd63bea3eb39343
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2009-05-13 13:36:38 +0000

    Tag the 1.1.0 final release.

---
+++ tags/1.1.0/lib/vcauth/forbidden/__init__.py
@@ -0,0 +1,46 @@
+# -*-python-*-
+#
+# Copyright (C) 2006-2008 The ViewCVS Group. All Rights Reserved.
+#
+# By using this file, you agree to the terms and conditions set forth in
+# the LICENSE.html file which can be found at the top level of the ViewVC
+# distribution or at http://viewvc.org/license-1.html.
+#
+# For more information, visit http://viewvc.org/
+#
+# -----------------------------------------------------------------------
+import vcauth
+import vclib
+import fnmatch
+import string
+
+class ViewVCAuthorizer(vcauth.GenericViewVCAuthorizer):
+  """A simple top-level module authorizer."""
+  def __init__(self, username, params={}):
+    forbidden = params.get('forbidden', '')
+    self.forbidden = map(string.strip,
+                         filter(None, string.split(forbidden, ',')))
+
+  def check_root_access(self, rootname):
+    return 1
+  
+  def check_path_access(self, rootname, path_parts, pathtype, rev=None):
+    # No path?  No problem.
+    if not path_parts:
+      return 1
+
+    # Not a directory?  We aren't interested.
+    if pathtype != vclib.DIR:
+      return 1
+
+    # At this point we're looking at a directory path.
+    module = path_parts[0]
+    default = 1
+    for pat in self.forbidden:
+      if pat[0] == '!':
+        default = 0
+        if fnmatch.fnmatchcase(module, pat[1:]):
+          return 1
+      elif fnmatch.fnmatchcase(module, pat):
+        return 0
+    return default
