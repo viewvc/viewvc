7965c5ee098bebe26e4964822779dfa0daf77274
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2009-05-13 14:15:53 +0000

    And the release cycle begins again...

--- branches/1.1.x/lib/viewvc.py
+++ branches/1.1.x/lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.1-dev'
+__version__ = '1.1.1-dev'
 
 # this comes from our library; measure the startup time
 import debug
