850b34950daca06dd4a21aa414810e027526324d
cmpilato <cmpilato@users.noreply.github.com>
2009-11-05 16:35:05 +0000

    Make ViewVC cross copies in Subversion history by default.
    * lib/config.py
      (Config.set_defaults): Make 'options.cross_copies' default to 1.  At
        a minimum that makes it jive with the commented out default value
        shown in viewvc.conf.dist, but I believe that it's also the better
        of the two ways to resolve that disparity in terms of user experience.
    
    Noticed by: Tom Throckmorton <throck{_AT_}gmail.com>

--- lib/config.py
+++ lib/config.py
@@ -240,7 +240,7 @@ class Config:
     self.options.show_subdir_lastmod = 0
     self.options.show_logs = 1
     self.options.show_log_in_markup = 1
-    self.options.cross_copies = 0
+    self.options.cross_copies = 1
     self.options.use_localtime = 0
     self.options.short_log_len = 80
     self.options.enable_syntax_coloration = 1
