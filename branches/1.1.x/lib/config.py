705a048d59f12fb751ea695e6954550e93a384dc
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2010-01-07 16:08:31 +0000

    Merge from trunk r2316, whose log message read like so:
       Add a bit of a workaround -- which fortunately applies to probably 99%
       of ViewVC users -- for issue #445 (standalone.py throws exception on
       root listing after visiting a root).
       
       * conf/viewvc.conf.dist
         Add warning about using per-root options with standalone.py.
       
       * lib/config.py
         (Config.overlay_root_options): Move the assertion/recording related
           to the one-time-only overlay stuffs down to the point where
           problems might occur.  This gives standalone.py users who don't
           use per-root configuration options the requisite mercy to let
           their servers continue to function.
    
    Also:
    
    * CHANGES
      Note this change.

--- branches/1.1.x/lib/config.py
+++ branches/1.1.x/lib/config.py
@@ -274,9 +274,7 @@ class Config:
     """Overlay per-root options for ROOTNAME atop the existing option
     set.  This is a destructive change to the configuration."""
 
-    # We can only deal with this happening once!
-    assert(self.root_options_overlayed == 0)
-    self.root_options_overlayed = 1
+    did_overlay = 0
     
     if not self.conf_path:
       return
@@ -285,7 +283,17 @@ class Config:
       base_section = self._is_allowed_override(self.parser, 'root',
                                                rootname, section)
       if base_section:
+        # We can currently only deal with root overlays happening
+        # once, so check that we've not yet done any overlaying of
+        # per-root options.
+        assert(self.root_options_overlayed == 0)
         self._process_section(self.parser, section, base_section)
+        did_overlay = 1
+
+    # If we actually did any overlaying, remember this fact so we
+    # don't do it again later.
+    if did_overlay:
+      self.root_options_overlayed = 1
 
   def _get_parser_items(self, parser, section):
     """Basically implement ConfigParser.items() for pre-Python-2.3 versions."""
