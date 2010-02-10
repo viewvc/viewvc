fef374ce4c7245aca21ae71ff6b08cf62f06d85b
cmpilato <cmpilato@users.noreply.github.com>
2010-02-10 19:23:06 +0000

    Merge from trunk r2325 and r2326:
       * lib/viewvc.py
         (query_backup): Switch to using the sapi interface for output file
           I/O instead of using 'print'.
         (view_queryform): Escape user-provided input before passing it
           directly off to the templates.  Can you say "CSS attack vector"?
    
    Also:
    
    * CHANGES
      Note the security fix.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -3621,23 +3621,26 @@ def view_queryform(request):
   limit_changes = \
     int(request.query_dict.get('limit_changes',
                                request.cfg.options.limit_changes))
-  
+
+  def escaped_query_dict_get(itemname, itemdefault=''):
+    return request.server.escape(request.query_dict.get(itemname, itemdefault))
+    
   data = common_template_data(request)
   data.merge(ezt.TemplateData({
-    'branch' : request.query_dict.get('branch', ''),
-    'branch_match' : request.query_dict.get('branch_match', 'exact'),
-    'dir' : request.query_dict.get('dir', ''),
-    'file' : request.query_dict.get('file', ''),
-    'file_match' : request.query_dict.get('file_match', 'exact'),
-    'who' : request.query_dict.get('who', ''),
-    'who_match' : request.query_dict.get('who_match', 'exact'),
-    'comment' : request.query_dict.get('comment', ''),
-    'comment_match' : request.query_dict.get('comment_match', 'exact'),
-    'querysort' : request.query_dict.get('querysort', 'date'),
-    'date' : request.query_dict.get('date', 'hours'),
-    'hours' : request.query_dict.get('hours', '2'),
-    'mindate' : request.query_dict.get('mindate', ''),
-    'maxdate' : request.query_dict.get('maxdate', ''),
+    'branch' : escaped_query_dict_get('branch', ''),
+    'branch_match' : escaped_query_dict_get('branch_match', 'exact'),
+    'dir' : escaped_query_dict_get('dir', ''),
+    'file' : escaped_query_dict_get('file', ''),
+    'file_match' : escaped_query_dict_get('file_match', 'exact'),
+    'who' : escaped_query_dict_get('who', ''),
+    'who_match' : escaped_query_dict_get('who_match', 'exact'),
+    'comment' : escaped_query_dict_get('comment', ''),
+    'comment_match' : escaped_query_dict_get('comment_match', 'exact'),
+    'querysort' : escaped_query_dict_get('querysort', 'date'),
+    'date' : escaped_query_dict_get('date', 'hours'),
+    'hours' : escaped_query_dict_get('hours', '2'),
+    'mindate' : escaped_query_dict_get('mindate', ''),
+    'maxdate' : escaped_query_dict_get('maxdate', ''),
     'query_action' : query_action,
     'query_hidden_values' : query_hidden_values,
     'limit_changes' : limit_changes,
@@ -3908,27 +3911,29 @@ def build_commit(request, files, max_files, dir_strip, format):
   return commit
 
 def query_backout(request, commits):
-  request.server.header('text/plain')
-  if commits:
-    print '# This page can be saved as a shell script and executed.'
-    print '# It should be run at the top of your work area.  It will update'
-    print '# your working copy to back out the changes selected by the'
-    print '# query.'
-    print
-  else:
-    print '# No changes were selected by the query.'
-    print '# There is nothing to back out.'
+  server_fp = get_writeready_server_file(request, 'text/plain')
+  if not commits:
+    server_fp.write("""\
+# No changes were selected by the query.
+# There is nothing to back out.
+""")
     return
+  server_fp.write("""\
+# This page can be saved as a shell script and executed.
+# It should be run at the top of your work area.  It will update
+# your working copy to back out the changes selected by the
+# query.
+""")
   for commit in commits:
     for fileinfo in commit.files:
       if request.roottype == 'cvs':
-        print 'cvs update -j %s -j %s %s/%s' \
-              % (fileinfo.rev, prev_rev(fileinfo.rev),
-                 fileinfo.dir, fileinfo.file)
+        server_fp.write('cvs update -j %s -j %s %s/%s\n'
+                        % (fileinfo.rev, prev_rev(fileinfo.rev),
+                           fileinfo.dir, fileinfo.file))
       elif request.roottype == 'svn':
-        print 'svn merge -r %s:%s %s/%s' \
-              % (fileinfo.rev, prev_rev(fileinfo.rev),
-                 fileinfo.dir, fileinfo.file)
+        server_fp.write('svn merge -r %s:%s %s/%s\n'
+                        % (fileinfo.rev, prev_rev(fileinfo.rev),
+                           fileinfo.dir, fileinfo.file))
 
 def view_query(request):
   if not is_query_supported(request):
