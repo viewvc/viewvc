cda129aa9317fd2f576e8d4a24a2f9e0653b91f6
cmpilato <cmpilato@users.noreply.github.com>
2010-03-10 21:48:46 +0000

    Schedule the 1.0.10 release.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.0.10-dev'
+__version__ = '1.0.10'
 
 # this comes from our library; measure the startup time
 import debug
