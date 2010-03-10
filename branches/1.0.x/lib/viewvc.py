221fff8b9c4368a66aab8999d2549ae470b0efcc
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2010-03-10 21:49:13 +0000

    Revert change accidentally committed in r2337.

--- branches/1.0.x/lib/viewvc.py
+++ branches/1.0.x/lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.0.10'
+__version__ = '1.0.10-dev'
 
 # this comes from our library; measure the startup time
 import debug
