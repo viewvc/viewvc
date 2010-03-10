7d84a802575d2921943a4a087eb345664159d911
cmpilato <cmpilato@users.noreply.github.com>
2010-03-10 21:50:25 +0000

    Begin a new release cycle.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.0.10-dev'
+__version__ = '1.0.11-dev'
 
 # this comes from our library; measure the startup time
 import debug
