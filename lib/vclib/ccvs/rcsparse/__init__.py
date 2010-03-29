395da004d27fffc452de1f20c4b121e0f5ad8d3d
C. Michael Pilato <cmpilato@red-bean.com>
2010-03-29 18:22:04 +0000

    * lib/vclib/ccvs/rcsparse/__init__.py, * lib/vclib/ccvs/rcsparse/common.py   Add some much-needed interface docs and comments.
    Patch by: Jon Foster <jon.foster {_AT_} cabot.co.uk>

--- lib/vclib/ccvs/rcsparse/__init__.py
+++ lib/vclib/ccvs/rcsparse/__init__.py
@@ -10,8 +10,17 @@
 #
 # -----------------------------------------------------------------------
 
-"""This package provides parsing tools for RCS files."""
+"""This package provides parsing tools for RCS files.
 
+To use this package, first create a subclass of Sink.  This should
+declare all the callback methods you care about.  Create an instance
+of your class, and open() the RCS file you want to read.  Then call
+parse() to parse the file.
+"""
+
+# Make the "Sink" class and the various exception classes visible in this
+# scope.  That way, applications never need to import any of the
+# sub-packages.
 from common import *
 
 try:
@@ -23,4 +32,13 @@ except ImportError:
     from default import Parser
 
   def parse(file, sink):
+    """Parse an RCS file.
+
+    Parameters: FILE is the file object to parse.  (I.e. an object of the
+    built-in Python type "file", usually created using Python's built-in
+    "open()" function).  It should be opened in binary mode.
+    SINK is an instance of (some subclass of) Sink.  It's methods will be
+    called as the file is parsed; see the definition of Sink for the
+    details.
+    """
     return Parser().parse(file, sink)
