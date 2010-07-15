1d0cad58ca33e585f60d2c3296606a5a5a762b0c
cmpilato <cmpilato@users.noreply.github.com>
2010-07-15 19:06:09 +0000

    Remove the GUI mode from standalone.py.  Great idea when the tool was simpler, but I've not done it justice in terms of upkeep.
    * bin/standalone.py
      (__author__, __date__, __version__, __credits__): Removed in favor of a
        a simple header comment.
      (Options.start_gui): Removed this member variable.
      (nogui, gui): Removed.
      (main): Was 'cli'.  Remove all option handling for, and usage
        references to, GUI mode.

--- bin/standalone.py
+++ bin/standalone.py
@@ -10,19 +10,13 @@
 # For more information, visit http://viewvc.org/
 #
 # -----------------------------------------------------------------------
+#
+# This program originally written by Peter Funk <pf@artcom-gmbh.de>, with
+# contributions by Ka-Ping Yee.
+#
+# -----------------------------------------------------------------------
 
-"""Run "standalone.py -p <port>" to start an HTTP server on a given port 
-on the local machine to generate ViewVC web pages.
-"""
-
-__author__ = "Peter Funk <pf@artcom-gmbh.de>"
-__date__ = "11 November 2001"
-__version__ = "$Revision$"
-__credits__ = """Guido van Rossum, for an excellent programming language.
-Greg Stein, for writing ViewCVS in the first place.
-Ka-Ping Yee, for the GUI code and the framework stolen from pydoc.py.
-"""
-
+#
 # INSTALL-TIME CONFIGURATION
 #
 # These values will be set during the installation process. During
@@ -55,7 +49,6 @@ import compat; compat.for_standalone()
 
 class Options:
   port = 49152      # default TCP/IP port used for the server
-  start_gui = 0     # No GUI unless requested.
   daemon = 0        # stay in the foreground by default
   repositories = {} # use default repositories specified in config
   host = sys.platform == 'mac' and '127.0.0.1' or 'localhost'
@@ -257,6 +250,7 @@ If this doesn't work, please click on the link above.
     else:
       self.log_error("ViewVC exited ok")
 
+
 class ViewVCHTTPServer(BaseHTTPServer.HTTPServer):
   """Customized HTTP server for ViewVC."""
   
@@ -283,6 +277,7 @@ class ViewVCHTTPServer(BaseHTTPServer.HTTPServer):
     if hasattr(socket, 'SOL_SOCKET') and hasattr(socket, 'SO_REUSEADDR'):
       self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     BaseHTTPServer.HTTPServer.server_bind(self)
+
 
 def serve(host, port, callback=None):
   """Start an HTTP server for HOST on PORT.  Call CALLBACK function
@@ -341,301 +336,23 @@ def serve(host, port, callback=None):
     pass
   print 'server stopped'
 
+
 def handle_config(config_file):
   global cfg
   cfg = viewvc.load_config(config_file or CONF_PATHNAME)
 
-# --- graphical interface: --------------------------------------------------
 
-def nogui(missing_module):
-  sys.stderr.write("""\
-Sorry! Your Python was compiled without the %s module enabled.
-I'm unable to run the GUI part.  Please omit the '-g' and '--gui' options,
-or install another Python interpreter.
-""" % (missing_module))
-  raise SystemExit, 1
-
-def gui(host, port):
-  """Graphical interface (starts web server and pops up a control window)."""
-  class GUI:
-    def __init__(self, window, host, port):
-      self.window = window
-      self.server = None
-      self.scanner = None
-
-      try:
-        import Tkinter
-      except ImportError:
-        nogui("Tkinter")
-
-      self.server_frm = Tkinter.Frame(window)
-      self.title_lbl = Tkinter.Label(self.server_frm,
-                                     text='Starting server...\n ')
-      self.open_btn = Tkinter.Button(self.server_frm,
-                                     text='open browser',
-                                     command=self.open,
-                                     state='disabled')
-      self.quit_btn = Tkinter.Button(self.server_frm,
-                                     text='quit serving',
-                                     command=self.quit,
-                                     state='disabled')
-
-      self.window.title('ViewVC standalone')
-      self.window.protocol('WM_DELETE_WINDOW', self.quit)
-      self.title_lbl.pack(side='top', fill='x')
-      self.open_btn.pack(side='left', fill='x', expand=1)
-      self.quit_btn.pack(side='right', fill='x', expand=1)
-
-      # Early loading of configuration here.  Used to
-      # allow tinkering with configuration settings through the gui:
-      handle_config(options.config_file)
-      if not LIBRARY_DIR:
-        cfg.options.cvsgraph_conf = "../cgi/cvsgraph.conf.dist"
-
-      self.options_frm = Tkinter.Frame(window)
-
-      # cvsgraph toggle:
-      self.cvsgraph_ivar = Tkinter.IntVar()
-      self.cvsgraph_ivar.set(cfg.options.use_cvsgraph)
-      self.cvsgraph_toggle = \
-          Tkinter.Checkbutton(self.options_frm,
-                              text="enable cvsgraph (needs binary)",
-                              var=self.cvsgraph_ivar,
-                              command=self.toggle_use_cvsgraph)
-      self.cvsgraph_toggle.pack(side='top', anchor='w')
-
-      # show_subdir_lastmod toggle:
-      self.subdirmod_ivar = Tkinter.IntVar()
-      self.subdirmod_ivar.set(cfg.options.show_subdir_lastmod)
-      self.subdirmod_toggle = \
-          Tkinter.Checkbutton(self.options_frm,
-                              text="show subdir last mod (dir view)",
-                              var=self.subdirmod_ivar,
-                              command=self.toggle_subdirmod)
-      self.subdirmod_toggle.pack(side='top', anchor='w')
-
-      # use_re_search toggle:
-      self.useresearch_ivar = Tkinter.IntVar()
-      self.useresearch_ivar.set(cfg.options.use_re_search)
-      self.useresearch_toggle = \
-          Tkinter.Checkbutton(self.options_frm,
-                              text="allow regular expr search",
-                              var=self.useresearch_ivar,
-                              command=self.toggle_useresearch)
-      self.useresearch_toggle.pack(side='top', anchor='w')
-
-      # use_localtime toggle:
-      self.use_localtime_ivar = Tkinter.IntVar()
-      self.use_localtime_ivar.set(cfg.options.use_localtime)
-      self.use_localtime_toggle = \
-          Tkinter.Checkbutton(self.options_frm,
-                              text="use localtime (instead of UTC)", 
-                              var=self.use_localtime_ivar,
-                              command=self.toggle_use_localtime)
-      self.use_localtime_toggle.pack(side='top', anchor='w')
-
-      # log_pagesize integer var:
-      self.log_pagesize_lbl = \
-          Tkinter.Label(self.options_frm,
-                        text='number of items per log page (0 disables):')
-      self.log_pagesize_lbl.pack(side='top', anchor='w')
-      self.log_pagesize_ivar = Tkinter.IntVar()
-      self.log_pagesize_ivar.set(cfg.options.log_pagesize)
-      self.log_pagesize_entry = \
-          Tkinter.Entry(self.options_frm,
-                        width=10,
-                        textvariable=self.log_pagesize_ivar)
-      self.log_pagesize_entry.bind('<Return>', self.set_log_pagesize)
-      self.log_pagesize_entry.pack(side='top', anchor='w')
-
-      # dir_pagesize integer var:
-      self.dir_pagesize_lbl = \
-          Tkinter.Label(self.options_frm,
-                        text='number of items per dir page (0 disables):')
-      self.dir_pagesize_lbl.pack(side='top', anchor='w')
-      self.dir_pagesize_ivar = Tkinter.IntVar()
-      self.dir_pagesize_ivar.set(cfg.options.dir_pagesize)
-      self.dir_pagesize_entry = \
-          Tkinter.Entry(self.options_frm,
-                        width=10,
-                        textvariable=self.dir_pagesize_ivar)
-      self.dir_pagesize_entry.bind('<Return>', self.set_dir_pagesize)
-      self.dir_pagesize_entry.pack(side='top', anchor='w')
-
-      # directory view template:
-      self.dirtemplate_lbl = \
-          Tkinter.Label(self.options_frm,
-                        text='Choose HTML Template for the Directory pages:')
-      self.dirtemplate_lbl.pack(side='top', anchor='w')
-      self.dirtemplate_svar = Tkinter.StringVar()
-      self.dirtemplate_svar.set(cfg.templates.directory)
-      self.dirtemplate_entry = \
-          Tkinter.Entry(self.options_frm,
-                        width=40,
-                        textvariable=self.dirtemplate_svar)
-      self.dirtemplate_entry.bind('<Return>', self.set_templates_directory)
-      self.dirtemplate_entry.pack(side='top', anchor='w')
-      self.templates_dir = \
-          Tkinter.Radiobutton(self.options_frm,
-                              text="directory.ezt",
-                              value="templates/directory.ezt", 
-                              var=self.dirtemplate_svar,
-                              command=self.set_templates_directory)
-      self.templates_dir.pack(side='top', anchor='w')
-      self.templates_dir_alt = \
-          Tkinter.Radiobutton(self.options_frm,
-                              text="dir_alternate.ezt",
-                              value="templates/dir_alternate.ezt", 
-                              var=self.dirtemplate_svar,
-                              command=self.set_templates_directory)
-      self.templates_dir_alt.pack(side='top', anchor='w')
-
-      # log view template:
-      self.logtemplate_lbl = \
-          Tkinter.Label(self.options_frm,
-                        text='Choose HTML Template for the Log pages:')
-      self.logtemplate_lbl.pack(side='top', anchor='w')
-      self.logtemplate_svar = Tkinter.StringVar()
-      self.logtemplate_svar.set(cfg.templates.log)
-      self.logtemplate_entry = \
-          Tkinter.Entry(self.options_frm,
-                        width=40,
-                        textvariable=self.logtemplate_svar)
-      self.logtemplate_entry.bind('<Return>', self.set_templates_log)
-      self.logtemplate_entry.pack(side='top', anchor='w')
-      self.templates_log = \
-          Tkinter.Radiobutton(self.options_frm,
-                              text="log.ezt",
-                              value="templates/log.ezt", 
-                              var=self.logtemplate_svar,
-                              command=self.set_templates_log)
-      self.templates_log.pack(side='top', anchor='w')
-      self.templates_log_table = \
-          Tkinter.Radiobutton(self.options_frm,
-                              text="log_table.ezt",
-                              value="templates/log_table.ezt", 
-                              var=self.logtemplate_svar,
-                              command=self.set_templates_log)
-      self.templates_log_table.pack(side='top', anchor='w')
-
-      # query view template:
-      self.querytemplate_lbl = \
-          Tkinter.Label(self.options_frm,
-                        text='Template for the database query page:')
-      self.querytemplate_lbl.pack(side='top', anchor='w')
-      self.querytemplate_svar = Tkinter.StringVar()
-      self.querytemplate_svar.set(cfg.templates.query)
-      self.querytemplate_entry = \
-          Tkinter.Entry(self.options_frm,
-                        width=40,
-                        textvariable=self.querytemplate_svar)
-      self.querytemplate_entry.bind('<Return>', self.set_templates_query)
-      self.querytemplate_entry.pack(side='top', anchor='w')
-      self.templates_query = \
-          Tkinter.Radiobutton(self.options_frm,
-                              text="query.ezt",
-                              value="templates/query.ezt", 
-                              var=self.querytemplate_svar,
-                              command=self.set_templates_query)
-      self.templates_query.pack(side='top', anchor='w')
-
-      # pack and set window manager hints:
-      self.server_frm.pack(side='top', fill='x')
-      self.options_frm.pack(side='top', fill='x')
-
-      self.window.update()
-      self.minwidth = self.window.winfo_width()
-      self.minheight = self.window.winfo_height()
-      self.expanded = 0
-      self.window.wm_geometry('%dx%d' % (self.minwidth, self.minheight))
-      self.window.wm_minsize(self.minwidth, self.minheight)
-
-      try:
-        import threading
-      except ImportError:
-        nogui("thread")
-      threading.Thread(target=serve, args=(host, port, self.ready)).start()
-
-    def toggle_use_cvsgraph(self, event=None):
-      cfg.options.use_cvsgraph = self.cvsgraph_ivar.get()
-
-    def toggle_use_localtime(self, event=None):
-      cfg.options.use_localtime = self.use_localtime_ivar.get()
-
-    def toggle_subdirmod(self, event=None):
-      cfg.options.show_subdir_lastmod = self.subdirmod_ivar.get()
-
-    def toggle_useresearch(self, event=None):
-      cfg.options.use_re_search = self.useresearch_ivar.get()
-
-    def set_log_pagesize(self, event=None):
-      cfg.options.log_pagesize = self.log_pagesize_ivar.get()
-
-    def set_dir_pagesize(self, event=None):
-      cfg.options.dir_pagesize = self.dir_pagesize_ivar.get()
-
-    def set_templates_log(self, event=None):
-      cfg.templates.log = self.logtemplate_svar.get()
-
-    def set_templates_directory(self, event=None):
-      cfg.templates.directory = self.dirtemplate_svar.get()
-
-    def set_templates_query(self, event=None):
-      cfg.templates.query = self.querytemplate_svar.get()
-
-    def ready(self, server):
-      """used as callback parameter to the serve() function"""
-      self.server = server
-      self.title_lbl.config(text='ViewVC standalone server at\n' + server.url)
-      self.open_btn.config(state='normal')
-      self.quit_btn.config(state='normal')
-
-    def open(self, event=None, url=None):
-      """opens a browser window on the local machine"""
-      url = url or self.server.url
-      try:
-        import webbrowser
-        webbrowser.open(url)
-      except ImportError: # pre-webbrowser.py compatibility
-        if sys.platform == 'win32':
-          os.system('start "%s"' % url)
-        elif sys.platform == 'mac':
-          try:
-            import ic
-            ic.launchurl(url)
-          except ImportError: pass
-        else:
-          rc = os.system('netscape -remote "openURL(%s)" &' % url)
-          if rc:
-            os.system('netscape "%s" &' % url)
-
-    def quit(self, event=None):
-      if self.server:
-        self.server.quit = 1
-      self.window.quit()
-
-  import Tkinter
-  try:
-    gui = GUI(Tkinter.Tk(), host, port)
-    Tkinter.mainloop()
-  except KeyboardInterrupt:
-    pass
-
-# --- command-line interface: ----------------------------------------------
-
-def cli(argv):
+def main(argv):
   """Command-line interface (looks at argv to decide what to do)."""
   import getopt
   class BadUsage(Exception): pass
 
   try:
     opts, args = getopt.getopt(argv[1:], 'gdc:p:r:h:s:', 
-                               ['gui', 'daemon', 'config-file=', 'host=',
+                               ['daemon', 'config-file=', 'host=',
                                 'port=', 'repository=', 'script-alias='])
     for opt, val in opts:
-      if opt in ('-g', '--gui'):
-        options.start_gui = 1
-      elif opt in ('-r', '--repository'):
+      if opt in ('-r', '--repository'):
         if options.repositories: # option may be used more than once:
           num = len(options.repositories.keys())+1
           symbolic_name = "Repository"+str(num)
@@ -656,22 +373,19 @@ def cli(argv):
           string.join(filter(None, string.split(val, '/')), '/')
       elif opt in ('-c', '--config-file'):
         options.config_file = val
-    if options.start_gui and options.config_file:
-      raise BadUsage, "--config-file option is not valid in GUI mode."
-    if not options.start_gui and not options.port:
-      raise BadUsage, "You must supply a valid port, or run in GUI mode."
+        
+    if not options.port:
+      raise BadUsage, "You must supply a valid port."
+    
     if options.daemon:
       pid = os.fork()
       if pid != 0:
-        sys.exit()  
-    if options.start_gui:
-      gui(options.host, options.port)
-      return
-    elif options.port:
-      def ready(server):
-        print 'server ready at %s%s' % (server.url, options.script_alias)
-      serve(options.host, options.port, ready)
-      return
+        sys.exit()
+        
+    def ready(server):
+      print 'server ready at %s%s' % (server.url, options.script_alias)
+    serve(options.host, options.port, ready)
+    return
   except (getopt.error, BadUsage), err:
     cmd = os.path.basename(sys.argv[0])
     port = options.port
@@ -690,7 +404,6 @@ Options:
                              file.  If not specified, ViewVC will try to use
                              the configuration file in its installation tree;
                              otherwise, built-in default values are used.
-                             (Not valid in GUI mode.)
                              
   --daemon (-d)              Background the server process.
   
@@ -711,12 +424,8 @@ Options:
                              "cgi-bin/viewvc", then ViewVC will be accessible
                              at "http://%(host)s:%(port)s/cgi-bin/viewvc".
                              [default: %(script_alias)s]
-  
-  --gui (-g)                 Pop up a graphical interface for serving and
-                             testing ViewVC.  NOTE: this requires a valid
-                             X11 display connection.
 """ % locals())
 
 if __name__ == '__main__':
   options = Options()
-  cli(sys.argv)
+  main(sys.argv)
