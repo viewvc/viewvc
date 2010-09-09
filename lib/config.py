159ab5d6f40a693f03829348229ae2b274b4500d
C. Michael Pilato <cmpilato@red-bean.com>
2010-09-09 13:40:42 +0000

    Allow users to tell the query script where ViewVC is located while still generating the same defaults as before.
    * conf/viewvc.conf.dist
      (query, query.viewvc_base_url): New section and value.
    
    * lib/config.py
      (_base_sections): Add 'query'.
      (Config.set_defaults): Set default value for cfg.query.viewvc_base_url.
    
    * bin/asp/query.asp,
    * bin/cgi/query.cgi,
    * bin/mod_python/query.py,
    * bin/wsgi/query.fcgi,
    * bin/wsgi/query.wsgi,
      Ask the configuration for the location of ViewVC before falling back
      to old defaults.
    
    * templates/query.ezt
      Don't reference unset variables.  Do test the log message for
      empty-ness, dropping a non-breaking space in place where it is.
    
    * templates/include/footer.ezt
      Don't reference unset variables.  
    
    * lib/query.py
      (build_commit): Allow ob.log to be empty.  Don't generate ViewVC
        links if we don't have a base URL for ViewVC.
      (main): Calculate docroot in light of possible absent viewvc_link.

--- lib/config.py
+++ lib/config.py
@@ -87,6 +87,11 @@ import fnmatch
 #                                           |  vhosts   |
 #                                           |           |
 #                                           `-----------'
+#                                           ,-----------.
+#                                           |           |
+#                                           |   query   |
+#                                           |           |
+#                                           `-----------'
 #
 # ### TODO:  Figure out what this all means for the 'kv' stuff.
 #
@@ -99,6 +104,7 @@ class Config:
     'cvsdb',
     'general',
     'options',
+    'query',
     'templates',
     'utilities',
     )
@@ -455,6 +461,8 @@ class Config:
     self.cvsdb.rss_row_limit = 100
     self.cvsdb.check_database_for_root = 0
 
+    self.query.viewvc_base_url = None
+    
 def _startswith(somestr, substr):
   return somestr[:len(substr)] == substr
 
