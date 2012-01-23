1310c7fc71602c438efb7ad5cbdbbabe56b11a2c
C. Michael Pilato <cmpilato@red-bean.com>
2012-01-23 17:52:47 +0000

    Bump copyright years.

--- lib/dbi.py
+++ lib/dbi.py
@@ -1,6 +1,6 @@
 # -*-python-*-
 #
-# Copyright (C) 1999-2006 The ViewCVS Group. All Rights Reserved.
+# Copyright (C) 1999-2012 The ViewCVS Group. All Rights Reserved.
 #
 # By using this file, you agree to the terms and conditions set forth in
 # the LICENSE.html file which can be found at the top level of the ViewVC
