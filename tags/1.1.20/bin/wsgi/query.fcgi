d6730a31a0cb77d97ad5d73314294ab4d7635dea
cmpilato <cmpilato@8cb11bc2-c004-0410-86c3-e597b4017df7>
2013-04-24 13:27:13 +0000

    Tag the 1.1.20 final release.

---
+++ tags/1.1.20/bin/wsgi/query.fcgi
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*-python-*-
+#
+# Copyright (C) 1999-2013 The ViewCVS Group. All Rights Reserved.
+#
+# By using this file, you agree to the terms and conditions set forth in
+# the LICENSE.html file which can be found at the top level of the ViewVC
+# distribution or at http://viewvc.org/license-1.html.
+#
+# For more information, visit http://viewvc.org/
+#
+# -----------------------------------------------------------------------
+#
+# viewvc: View CVS/SVN repositories via a web browser
+#
+# -----------------------------------------------------------------------
+#
+# This is a fcgi entry point for the query ViewVC app. It's appropriate
+# for use with mod_fcgid and flup. It defines a single application function
+# that is a valid fcgi entry point.
+#
+# mod_fcgid: http://httpd.apache.org/mod_fcgid/
+# flup:
+#   http://pypi.python.org/pypi/flup
+#   http://trac.saddi.com/flup
+#
+# -----------------------------------------------------------------------
+
+import sys, os
+
+LIBRARY_DIR = None
+CONF_PATHNAME = None
+
+if LIBRARY_DIR:
+  sys.path.insert(0, LIBRARY_DIR)
+else:
+  sys.path.insert(0, os.path.abspath(os.path.join(sys.argv[0],
+                                                  "../../../lib")))
+
+import sapi
+import viewvc
+import query
+from flup.server import fcgi
+
+def application(environ, start_response):
+  server = sapi.WsgiServer(environ, start_response)
+  cfg = viewvc.load_config(CONF_PATHNAME, server)
+  viewvc_base_url = cfg.query.viewvc_base_url
+  if viewvc_base_url is None:
+    viewvc_base_url = "viewvc.fcgi"
+  query.main(server, cfg, viewvc_base_url)
+  return []
+
+fcgi.WSGIServer(application).run()
---
+++ tags/1.1.20/lib/vclib/ccvs/__init__.py
@@ -0,0 +1,62 @@
+# -*-python-*-
+#
+# Copyright (C) 1999-2013 The ViewCVS Group. All Rights Reserved.
+#
+# By using this file, you agree to the terms and conditions set forth in
+# the LICENSE.html file which can be found at the top level of the ViewVC
+# distribution or at http://viewvc.org/license-1.html.
+#
+# For more information, visit http://viewvc.org/
+#
+# -----------------------------------------------------------------------
+import os
+import os.path
+
+
+def canonicalize_rootpath(rootpath):
+  assert os.path.isabs(rootpath)
+  return os.path.normpath(rootpath)
+
+
+def _is_cvsroot(path):
+  return os.path.exists(os.path.join(path, "CVSROOT", "config"))
+
+
+def expand_root_parent(parent_path):
+  # Each subdirectory of PARENT_PATH that contains a child
+  # "CVSROOT/config" is added the set of returned roots.  Or, if the
+  # PARENT_PATH itself contains a child "CVSROOT/config", then all its
+  # subdirectories are returned as roots.
+  assert os.path.isabs(parent_path)
+  roots = {}
+  subpaths = os.listdir(parent_path)
+  for rootname in subpaths:
+    rootpath = os.path.join(parent_path, rootname)
+    if _is_cvsroot(parent_path) or _is_cvsroot(rootpath):
+      roots[rootname] = canonicalize_rootpath(rootpath)
+  return roots
+
+
+def find_root_in_parent(parent_path, rootname):
+  """Search PARENT_PATH for a root named ROOTNAME, returning the
+  canonicalized ROOTPATH of the root if found; return None if no such
+  root is found."""
+  # Is PARENT_PATH itself a CVS repository?  If so, we allow ROOTNAME
+  # to be any subdir within it.  Otherwise, we expect
+  # PARENT_PATH/ROOTNAME to be a CVS repository.
+  assert os.path.isabs(parent_path)
+  rootpath = os.path.join(parent_path, rootname)
+  if (_is_cvsroot(parent_path) and os.path.exists(rootpath)) \
+     or _is_cvsroot(rootpath):
+    return canonicalize_rootpath(rootpath)
+  return None
+
+
+def CVSRepository(name, rootpath, authorizer, utilities, use_rcsparse):
+  rootpath = canonicalize_rootpath(rootpath)
+  if use_rcsparse:
+    import ccvs
+    return ccvs.CCVSRepository(name, rootpath, authorizer, utilities)
+  else:
+    import bincvs
+    return bincvs.BinCVSRepository(name, rootpath, authorizer, utilities)
