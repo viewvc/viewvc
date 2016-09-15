46e0f77dfdc09c8d9b03e900c70dfe5a3e3c6c37
C. Michael Pilato <cmpilato@collab.net>
2016-09-15 15:44:18 -0400

    Begin a new release cycle.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.1.25'
+__version__ = '1.1.26-dev'
 
 # this comes from our library; measure the startup time
 import debug
