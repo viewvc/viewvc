776e7d9d95f7c51be2c739f7cefd1469f4d266d4
FUTATSUKI Yasuhito <futatuki@yf.bsdclub.org>
2018-11-04 01:00:18 +0900

    Enable to display Exception other than ViewVCException

--- lib/debug.py
+++ lib/debug.py
@@ -113,6 +113,9 @@ def GetExceptionData():
     if isinstance(exc, ViewVCException):
       exc_dict['msg'] = exc.msg
       exc_dict['status'] = exc.status
+    elif isinstance(exc, Exception):
+      exc_dict['msg'] = str(exc)
+      exc_dict['status'] = '500 Internal Server Error'
 
     tb = ''.join(traceback.format_exception(exc_type, exc, exc_tb))
     exc_dict['stacktrace'] = tb
