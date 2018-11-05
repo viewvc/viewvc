0165abcf15372273bbb49ae25067312fd8e8e6b7
C. Michael Pilato <cmpilato@collab.net>
2018-11-05 16:07:02 -0500

    Issue #169: Show directory and root counts, too
    * docs/template-authoring-guide.html
      Document new 'roots_shown' and 'dirs_shown' variables.
    
    * lib/viewvc.py
      (view_roots): Calculate new 'roots_shown' template data variable.
      (view_directory): Calculate new 'dirs_shown' template data variable.
    
    * templates/default/directory.ezt
      Show directory count, too.
    
    * templates/default/roots.ezt
      Show root count.
    
    * templates/default/docroot/styles.css,
    * templates/default/include/diff_form.ezt
      Unrelated changes.  So sue me.
    
    Suggested by: danielmarschall

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -2253,6 +2253,7 @@ def view_roots(request):
   data = common_template_data(request)
   data.merge(TemplateData({
     'roots' : roots,
+    'roots_shown' : len(roots), 
     }))
   generate_page(request, "roots", data)
 
@@ -2326,7 +2327,7 @@ def view_directory(request):
 
   # loop through entries creating rows and changing these values
   rows = [ ]
-  num_displayed = 0
+  dirs_displayed = files_displayed = 0
   num_dead = 0
   
   # set some values to be used inside loop
@@ -2369,6 +2370,8 @@ def view_directory(request):
                              request.path_parts + [file.name]):
         continue
     
+      dirs_displayed += 1
+
       row.view_href = request.get_url(view_func=view_directory,
                                       where=where_prefix+file.name,
                                       pathtype=vclib.DIR,
@@ -2405,7 +2408,7 @@ def view_directory(request):
         if hideattic:
           continue
         
-      num_displayed = num_displayed + 1
+      files_displayed += 1
 
       file_where = where_prefix + file.name
       if request.roottype == 'svn': 
@@ -2460,7 +2463,8 @@ def view_directory(request):
     'sortby_log_href' :    request.get_url(params={'sortby': 'log',
                                                    'sortdir': None},
                                            escape=1),
-    'files_shown' : num_displayed,
+    'files_shown' : files_displayed,
+    'dirs_shown' : dirs_displayed,
     'num_dead' : num_dead,
     'youngest_rev' : None,
     'youngest_rev_href' : None,
