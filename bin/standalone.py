238c44f7635bc2a3a7f9e15e14081b9ef5b15212
FUTATSUKI Yasuhito <futatuki@yf.bsdclub.org>
2018-11-06 00:15:21 +0900

    avoid double fault to report error after closing stdout

--- bin/standalone.py
+++ bin/standalone.py
@@ -360,6 +360,17 @@ class ViewVCHTTPServer(BaseHTTPServer.HTTPServer):
       self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     BaseHTTPServer.HTTPServer.server_bind(self)
 
+  def handle_error(self, request, client_address):
+    """Handle an error gracefully. use stderr instead of stdout
+    to avoid double fault.
+    """
+    print >>sys.stderr, '-'*40
+    print >>sys.stderr, 'Exception happened during processing of request from',
+    print >>sys.stderr, client_address
+    import traceback
+    traceback.print_exc()
+    print >>sys.stderr, '-'*40
+
 
 def serve(host, port, callback=None):
   """Start an HTTP server for HOST on PORT.  Call CALLBACK function
