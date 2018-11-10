43a340aaaa0a5e1b013c0c56c156fa47b4f5a0fa
C. Michael Pilato <cmpilato@collab.net>
2018-11-10 17:22:43 -0500

    Issue #175: Show unique icon in directory list for viewable images
    * docs/template-authoring-guide.html
      Note new 'is_binary' and 'is_viewable_image' values on directory
      view entries.
    
    * lib/viewvc.py
      (get_file_view_info): Now calculate and return on the file view info
        object is_viewable_image and is_binary values, too.
    
    * templates/classic/docroot/images/image.png,
    * templates/default/docroot/images/image.png
      New "viewable image" icon.
    
    * templates/classic/directory.ezt
    * templates/default/directory.ezt
      Update template logic to show the new image icon for viewable image
      files.
    
    Suggested by: danielmarschall

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -1107,7 +1107,9 @@ def get_file_view_info(request, where, rev=None, mime_type=None, pathrev=-1):
      annotate_href
      revision_href
      prefer_markup
-
+     is_viewable_image
+     is_binary
+     
   """
 
   rev = rev and str(rev) or None
@@ -1167,7 +1169,9 @@ def get_file_view_info(request, where, rev=None, mime_type=None, pathrev=-1):
                download_text_href=download_text_href,
                annotate_href=annotate_href,
                revision_href=revision_href,
-               prefer_markup=ezt.boolean(prefer_markup))
+               prefer_markup=ezt.boolean(prefer_markup),
+               is_viewable_image=ezt.boolean(is_viewable_image(mime_type)),
+               is_binary=ezt.boolean(is_binary_file))
 
 
 # Matches URLs
@@ -2383,7 +2387,8 @@ def view_directory(request):
                 log_file=None, log_rev=None, graph_href=None, mime_type=None,
                 date=None, ago=None, view_href=None, log_href=None,
                 revision_href=None, annotate_href=None, download_href=None,
-                download_text_href=None, prefer_markup=ezt.boolean(0))
+                download_text_href=None, prefer_markup=ezt.boolean(0),
+                is_viewable_image=ezt.boolean(0), is_binary=ezt.boolean(0))
     if request.roottype == 'cvs' and file.absent:
       continue
     if cfg.options.hide_errorful_entries and file.errors:
@@ -2467,6 +2472,8 @@ def view_directory(request):
       row.annotate_href = fvi.annotate_href
       row.revision_href = fvi.revision_href
       row.prefer_markup = fvi.prefer_markup
+      row.is_viewable_image = fvi.is_viewable_image
+      row.is_binary = fvi.is_binary
       row.log_href = request.get_url(view_func=view_log,
                                      where=file_where,
                                      pathtype=vclib.FILE,
