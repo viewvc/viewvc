3ab6e43ddb3096749c0de4706b8514486c8ba92f
C. Michael Pilato <cmpilato@collab.net>
2020-03-17 14:04:41 -0400

    Prepare for the 1.2.0 release

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.2.0-dev'
+__version__ = '1.2.0'
 
 # this comes from our library; measure the startup time
 import debug
