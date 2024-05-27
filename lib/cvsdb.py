6da7e99ea4bdf2cfced3e555650751dd872f3710
Mark A. Ziesemer <online@mark.ziesemer.com>
2024-05-26 19:34:45 -0500

    Revert "Revert dirs to binary, handle conversion in Python instead."
    This reverts commit c29f2d605dc43aea26426865ff57d70e9af0972f.

--- lib/cvsdb.py
+++ lib/cvsdb.py
@@ -90,8 +90,7 @@ class CheckinDatabase:
 
     def sql_get_id(self, table, column, value, auto_set):
         value = reencode(value)
-        binary_op = "BINARY " if column in ('dir',) else ""
-        sql = "SELECT id FROM %s WHERE %s=%s%%s" % (table, column, binary_op)
+        sql = "SELECT id FROM %s WHERE %s=%%s" % (table, column)
         sql_args = (value,)
 
         cursor = self.db.cursor()
@@ -105,7 +104,7 @@ class CheckinDatabase:
             return str(int(id))
 
         # insert the new identifier
-        sql = "INSERT INTO %s(%s) VALUES(%s%%s)" % (table, column, binary_op)
+        sql = "INSERT INTO %s(%s) VALUES(%%s)" % (table, column)
         sql_args = (value,)
         cursor.execute(sql, sql_args)
 
