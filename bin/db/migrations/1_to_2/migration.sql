c79fe54e80028d71d1e793f0d81d320242a6b5a3
C. Michael Pilato <cmpilato@digital.ai>
2025-07-11 22:06:42 -0400

    Use utf8mb4 for schemas and migrations

--- bin/db/migrations/1_to_2/migration.sql
+++ bin/db/migrations/1_to_2/migration.sql
@@ -3,49 +3,49 @@
 -- Convert branches table to InnoDB with UTF-8
 ALTER TABLE branches 
     ENGINE=InnoDB,
-    MODIFY COLUMN branch varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' NOT NULL;
+    MODIFY COLUMN branch varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '' NOT NULL;
 
 -- Convert commits table to InnoDB with UTF-8
 ALTER TABLE commits 
     ENGINE=InnoDB,
-    MODIFY COLUMN revision varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
-    MODIFY COLUMN stickytag varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '';
+    MODIFY COLUMN revision varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+    MODIFY COLUMN stickytag varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '';
 
 -- Convert descs table to InnoDB with UTF-8
 ALTER TABLE descs 
     ENGINE=InnoDB,
-    MODIFY COLUMN description text CHARACTER SET utf8 COLLATE utf8_bin;
+    MODIFY COLUMN description text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- Convert dirs table to InnoDB with UTF-8
 ALTER TABLE dirs 
     ENGINE=InnoDB,
-    MODIFY COLUMN dir varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' NOT NULL;
+    MODIFY COLUMN dir varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '' NOT NULL;
 
 -- Convert files table to InnoDB with UTF-8
 ALTER TABLE files 
     ENGINE=InnoDB,
-    MODIFY COLUMN file varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' NOT NULL;
+    MODIFY COLUMN file varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '' NOT NULL;
 
 -- Convert people table to InnoDB with UTF-8
 ALTER TABLE people 
     ENGINE=InnoDB,
-    MODIFY COLUMN who varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' NOT NULL;
+    MODIFY COLUMN who varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '' NOT NULL;
 
 -- Convert repositories table to InnoDB with UTF-8
 ALTER TABLE repositories 
     ENGINE=InnoDB,
-    MODIFY COLUMN repository varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' NOT NULL;
+    MODIFY COLUMN repository varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '' NOT NULL;
 
 -- Convert tags table to InnoDB with UTF-8
 ALTER TABLE tags 
     ENGINE=InnoDB,
-    MODIFY COLUMN revision varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' NOT NULL;
+    MODIFY COLUMN revision varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '' NOT NULL;
 
 -- Convert metadata table to InnoDB with UTF-8
 ALTER TABLE metadata 
     ENGINE=InnoDB,
-    MODIFY COLUMN name varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' NOT NULL,
-    MODIFY COLUMN value text CHARACTER SET utf8 COLLATE utf8_bin;
+    MODIFY COLUMN name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '' NOT NULL,
+    MODIFY COLUMN value text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- Update schema version to 2
 UPDATE metadata SET value = '2' WHERE name = 'version';
