0fe123924c4147c2e68247a1d5381fdf730b1563
C. Michael Pilato <cmpilato@digital.ai>
2025-07-21 20:48:29 -0400

    Begin a new release cycle.

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.1.31'
+__version__ = '1.1.32-dev'
 
 # this comes from our library; measure the startup time
 import debug
