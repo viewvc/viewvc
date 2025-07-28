0ae3f368a3a032f013fbf995f10311ed65ff2397
C. Michael Pilato <cmpilato@digital.ai>
2025-07-28 07:47:15 -0400

    Prepare for the 1.2.5 release

--- lib/viewvc.py
+++ lib/viewvc.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 
-__version__ = '1.2.5-dev'
+__version__ = '1.2.5'
 
 # this comes from our library; measure the startup time
 import debug
